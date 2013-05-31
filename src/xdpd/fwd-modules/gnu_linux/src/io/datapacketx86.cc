333c8877aea5519fd0ec8c9502d1b278e24e2d00
Marc Sune <marc.sune@bisdn.de>
2013-05-31 14:49:39 +0200

    Making rofl packet classifier the default one

--- src/xdpd/fwd-modules/gnu_linux/src/io/datapacketx86.cc
+++ src/xdpd/fwd-modules/gnu_linux/src/io/datapacketx86.cc
@@ -9,8 +9,8 @@
  */
 
 //Change this if you want to use another scheduler
-//typedef rofl_pktclassifier pktclassifier;
-typedef static_pktclassifier pktclassifier;
+typedef rofl_pktclassifier pktclassifier;
+//typedef static_pktclassifier pktclassifier;
 
 //Constructor
 datapacketx86::datapacketx86() :

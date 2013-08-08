9e1428959ee7013c0d7b88cf0b5f529349ac6b03
Marc Sune <marc.sune@bisdn.de>
2013-08-08 16:11:50 +0200

    Adding necessary components from GNU/Linux fwd_module into the netfpga10g one (symlinking)

--- src/xdpd/fwd-modules/netfpga10g/src/util/compiler_assert.h
+++ src/xdpd/fwd-modules/netfpga10g/src/util/compiler_assert.h
@@ -1,10 +1,1 @@
-#include <assert.h>
-
-#ifndef _COMPILER_ASSERT_H_
-#define _COMPILER_ASSERT_H_
-
-
-#define COMPILER_ASSERT(tag,cond) \
-	enum { COMPILER_ASSERT__ ## tag = 1/(cond) }
-
-#endif //COMPILER_ASSERT_H
+../../../gnu_linux/src/util/compiler_assert.h

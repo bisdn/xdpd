8a1a3400374ddd34b9d22f4d779b7b7c31c2850c
Marc Sune <marc.sune@bisdn.de>
2013-08-08 15:53:15 +0200

    Adding some comments on the packet in

--- src/xdpd/fwd-modules/netfpga10g/src/io/packet_io.c
+++ src/xdpd/fwd-modules/netfpga10g/src/io/packet_io.c
@@ -3,9 +3,20 @@
 #define FWD_MOD_NAME "netfpga10g"
 
 void netpfga_io_read_from_port(switch_port_t* port){
-	
-	ROFL_ERR("["FWD_MOD_NAME"] Got a packet from kernel (PKT_IN)!\n");
 
-	//FIXME implement!	
+	netfpga_port_t* state = (netfpga_port_t*)port->platform_port_state;
+
+	ROFL_ERR("["FWD_MOD_NAME"] Got a packet from kernel (PKT_IN) in port %s!\n", port->name);
+
+	//Retrieve an empty buffer
+
+	//Non-blocking read of packets
+	//read(state->fd, buffer, size);
+	//classify it
+
+	//Store the packet in storage	
+
+	//Call CMM cmm_of12_packet_in();
+	
 	
 }

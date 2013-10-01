89ba54c111ff0bd53a6908ebe465c0d0755ddfd8
Marc Sune <marc.sune@bisdn.de>
2013-10-01 14:08:11 +0200

    Deleting invalid return statement on a method returning void

--- src/xdpd/openflow/openflow10/openflow10_switch.cc
+++ src/xdpd/openflow/openflow10/openflow10_switch.cc
@@ -93,11 +93,11 @@ afa_result_t openflow10_switch::notify_port_status_changed(switch_port_t* port){
 * Connecting and disconnecting from a controller entity
 */
 void openflow10_switch::rpc_connect_to_ctl(caddress const& controller_addr){
-	return endpoint->rpc_connect_to_ctl(ofswitch->of_ver, 0, controller_addr);
+	endpoint->rpc_connect_to_ctl(ofswitch->of_ver, 0, controller_addr);
 }
 
 void openflow10_switch::rpc_disconnect_from_ctl(caddress const& controller_addr){
-	return endpoint->rpc_disconnect_from_ctl(controller_addr);
+	endpoint->rpc_disconnect_from_ctl(controller_addr);
 }
 
 

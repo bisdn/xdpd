a9f9728dcf0d9c924b54c07bacc47df52ee52108
Marc Sune <marc.sune@bisdn.de>
2013-10-03 17:21:02 +0200

    deleting old checking for OF1.2

--- src/xdpd/fwd-modules/gnu_linux/src/afa-imp/openflow/openflow1x/of1x_forwarding_module.cc
+++ src/xdpd/fwd-modules/gnu_linux/src/afa-imp/openflow/openflow1x/of1x_forwarding_module.cc
@@ -207,10 +207,7 @@ afa_result_t fwd_module_of1x_set_table_config(uint64_t dpid, unsigned int table_
 	lsw = (of1x_switch_t*)physical_switch_get_logical_switch_by_dpid(dpid);
 
 	//Check switch and port
-	if(!lsw || 
-		lsw->of_ver != OF_VERSION_12 || 
-		( (table_id != OF1X_FLOW_TABLE_ALL) && (table_id >= lsw->pipeline->num_of_tables) )
-	) {
+	if( !lsw || ( (table_id != OF1X_FLOW_TABLE_ALL) && (table_id >= lsw->pipeline->num_of_tables) ) ) {
 		//TODO: log this... should never happen
 		assert(0);
 		return AFA_FAILURE;

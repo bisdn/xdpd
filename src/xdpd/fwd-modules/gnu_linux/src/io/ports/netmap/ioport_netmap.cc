fbb9ca8b11868f20e6abef355c91c6dbb186a6bb
Marc Sune <marc.sune@bisdn.de>
2013-10-19 14:19:20 +0200

    Adding netmap ioport skeleton

---
+++ src/xdpd/fwd-modules/gnu_linux/src/io/ports/netmap/ioport_netmap.cc
@@ -0,0 +1,131 @@
+#include "ioport_netmap.h"
+#include <iostream>
+#include <rofl/common/utils/c_logger.h>
+#include "../../bufferpool.h" 
+#include <fcntl.h>
+
+using namespace xdpd::gnu_linux;
+
+//Constructor and destructor
+ioport_netmap::ioport_netmap(switch_port_t* of_ps, unsigned int num_queues):ioport(of_ps,num_queues){
+	
+	int ret,flags,i;
+
+	//Open pipe to simulate socket input fd 
+	ret = pipe(input);
+	(void)ret; // todo use the value
+
+
+	//Mark READ end as non-blocking for read
+	flags = fcntl(input[READ], F_GETFL, 0);		//get current file status flags
+	flags |= O_NONBLOCK;				//turn off blocking flag
+	fcntl(input[READ], F_SETFL, flags);		//set up non-blocking read
+		
+	//Open pipe for output signaling on enqueue	
+	ret = pipe(notify_pipe);
+	(void)ret; // todo use the value
+
+	//Set non-blocking read/write in the pipe
+	for(i=0;i<2;i++){
+		flags = fcntl(notify_pipe[i], F_GETFL, 0);	///get current file status flags
+		flags |= O_NONBLOCK;				//turn off blocking flag
+		fcntl(notify_pipe[i], F_SETFL, flags);		//set up non-blocking read
+	}
+
+}
+
+ioport_netmap::~ioport_netmap(){
+	close(input[READ]);
+	close(input[WRITE]);
+	close(notify_pipe[READ]);
+	close(notify_pipe[WRITE]);
+}
+
+//Read and write methods over port
+void ioport_netmap::enqueue_packet(datapacket_t* pkt, unsigned int q_id){
+
+	size_t ret;
+	//Whatever
+	const char c='a';
+
+	//Put in the queue
+	output_queues[q_id].blocking_write(pkt);
+
+	//TODO: make it happen only if thread is really sleeping...
+	ret = ::write(notify_pipe[WRITE],&c,sizeof(c));
+	(void)ret; // todo use the value
+
+}
+
+datapacket_t* ioport_netmap::read(){
+	
+	datapacket_t* pkt; 
+	datapacketx86* pkt_x86;
+
+
+	//First attempt drain local buffers from previous reads that failed to push 
+	pkt = input_queue.non_blocking_read();	
+	if(pkt)
+		return pkt;		
+
+	//Allocate free buffer	
+	pkt = bufferpool::get_free_buffer();
+	pkt_x86 = ((datapacketx86*)pkt->platform_state);
+
+	//Init in user space
+	pkt_x86->init(NULL, SIMULATED_PKT_SIZE, of_port_state->attached_sw, of_port_state->of_port_num);
+	
+	//copy something from stdin to buffer
+	if(::read(input[READ],pkt_x86->get_buffer(),SIMULATED_PKT_SIZE) <0){
+		bufferpool::release_buffer(pkt);
+		return NULL;
+	}
+
+	ROFL_DEBUG_VERBOSE("Filled buffer with id:%d. Sending to process.\n", pkt_x86->buffer_id);
+	
+	return pkt;	
+}
+
+
+unsigned int ioport_netmap::write(unsigned int q_id, unsigned int num_of_buckets){
+
+	size_t ret;
+	uint8_t dummy[SIMULATED_PKT_SIZE];
+	unsigned int i;
+	datapacket_t* pkt;
+	datapacketx86* pkt_x86;
+	
+	(void)pkt_x86;
+
+	//Free the pipe
+	ret = ::read(notify_pipe[READ],&dummy,SIMULATED_PKT_SIZE);
+	(void)ret; // todo use the value
+
+	//Go and do stuff
+	for(i=0;i<num_of_buckets;i++){	
+		//Pick buffer	
+		pkt = output_queues[q_id].non_blocking_read();
+		
+		if(!pkt)
+			break;
+		
+		//Just "put it into the wire" -> print it 
+		pkt_x86 = (datapacketx86*)pkt->platform_state;
+		
+		ROFL_DEBUG_VERBOSE("Getting buffer with id:%d. Putting it into the wire\n", pkt_x86->buffer_id);
+		
+		//Free buffer
+		bufferpool::release_buffer(pkt);
+	}
+	i++;
+	return num_of_buckets-i;
+
+}
+
+rofl_result_t ioport_netmap::disable(){
+	return ROFL_SUCCESS;
+}
+
+rofl_result_t ioport_netmap::enable(){
+	return ROFL_SUCCESS;
+}
---
+++ src/xdpd/fwd-modules/gnu_linux/src/io/ports/netmap/ioport_netmap.h
@@ -0,0 +1,73 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef IOPORT_NETMAP_H
+#define IOPORT_NETMAP_H 
+
+#include <unistd.h>
+#include <rofl.h>
+#include <rofl/datapath/pipeline/common/datapacket.h>
+#include <rofl/datapath/pipeline/switch_port.h>
+#include "../ioport.h" 
+#include "../../datapacketx86.h" 
+
+
+namespace xdpd {
+namespace gnu_linux {
+
+/**
+* @brief Simple netmap of a port, used for testing purposes only.
+* 
+* It opens two files, on for input and another for output.
+*/
+class ioport_netmap : public ioport{
+
+public:
+	//ioport_netmap
+	ioport_netmap(switch_port_t* of_ps, unsigned int num_queues=MMAP_DEFAULT_NUM_OF_QUEUES);
+	virtual ~ioport_netmap();
+	 
+	//Enque packet for transmission (blocking)
+	virtual void enqueue_packet(datapacket_t* pkt, unsigned int q_id);
+
+	//Non-blocking read and write
+	virtual datapacket_t* read(void);
+	virtual unsigned int write(unsigned int q_id, unsigned int num_of_buckets);
+
+	//Get read&write fds. Return -1 if do not exist
+	inline virtual int get_read_fd(void){return input[READ];};
+	int get_fake_write_fd(void){return input[WRITE];};
+	inline virtual int get_write_fd(void){return notify_pipe[READ];};
+
+	//Get buffer status
+	//virtual circular_queue_state_t get_input_queue_state(void); 
+	//virtual circular_queue_state_t get_output_queue_state(unsigned int q_id=0);
+
+	virtual rofl_result_t 
+	disable();
+
+	virtual rofl_result_t
+	enable();
+
+	static const size_t SIMULATED_PKT_SIZE=1500;
+
+protected:
+	//Queues
+	static const unsigned int MMAP_DEFAULT_NUM_OF_QUEUES=8; 
+
+	//fds
+	int input[2];
+	int notify_pipe[2];
+	
+	//Pipe extremes
+	static const unsigned int READ=0;
+	static const unsigned int WRITE=1;
+	
+};
+
+}// namespace xdpd::gnu_linux 
+}// namespace xdpd
+
+
+#endif /* IOPORT_NETMAP_H_ */

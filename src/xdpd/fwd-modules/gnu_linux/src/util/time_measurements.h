68bb734fc334def46cb08c556f05cfe301c4425a
Marc Sune <marc.sune@bisdn.de>
2013-11-04 13:38:52 +0100

    mc

--- src/xdpd/fwd-modules/gnu_linux/src/util/time_measurements.h
+++ src/xdpd/fwd-modules/gnu_linux/src/util/time_measurements.h
@@ -112,96 +112,113 @@ extern time_measurements_t global_measurements;
 
 	inline void tm_stamp_stage(time_measurements_t* tm, time_stages_t stage){
 
-		uint64_t now = 1;
+		uint64_t now = rdtsc();
 
-		now = rdtsc();
-		
 		switch(stage){
 			case TM_S0:
 				memset(tm->current,0,sizeof(uint64_t)*TM_MAX);
 				tm->s2_reached = false;
 				tm->s5_reached = false;
 				tm->sb6_reached = false;
+				//fprintf(stderr, "************\n");
 				break;
 			case TM_S1:
 				assert(tm->current[TM_S0] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_S0];
+				//fprintf(stderr, "S1 now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S0]);
 				break;
 			case TM_S2:
 				assert(tm->current[TM_S1] != 0.0);
 				if(!tm->s5_reached){ //May be called more than one time
 					tm->accumulated[stage] += now - tm->current[TM_S1];
 					tm->s2_reached = true;
+					//fprintf(stderr, "S2 now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S1]);
 				}
 				break;
 			case TM_S3_PRE:
 				assert(tm->current[TM_S2] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_S2];
+				//fprintf(stderr, "S3_PRE now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S2]);
 				break;
 			case TM_S3_SUCCESS:
 				assert(tm->current[TM_S3_PRE] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_S3_PRE];
+				//fprintf(stderr, "S3_SUCCESS now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S3_PRE]);
 				break;
 			case TM_S3_FAILURE:
 				assert(tm->current[TM_S3_PRE] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_S3_PRE];
 				tm->total_S3_dropped_pkts++;
 				tm->total_pkts++;
+				//fprintf(stderr, "S3_FAILURE now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S3_PRE]);
 				break;
 			case TM_S4:
 				assert(tm->current[TM_S3_PRE] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_S3_PRE];		//Avoid race condition
+				//fprintf(stderr, "S4 now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S3_PRE]);
 				break;
 			case TM_S5:
 				assert(tm->current[TM_S4] != 0.0);
 				if(!tm->s5_reached){ //May be called more than one time
 					tm->accumulated[stage] += now - tm->current[TM_S4];
 					tm->s5_reached = true;
+					//fprintf(stderr, "S5 now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S4]);
 				}
 				break;
 			case TM_SA6_PRE:
 				assert(tm->current[TM_S5] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_S5];
+				//fprintf(stderr, "SA6_PRE now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S5]);
 				break;
 			case TM_SA6_SUCCESS:
-				if(tm->current[TM_SA6_PRE] != 0.0) //PKT_OUT or flow_mod
+				if(tm->current[TM_SA6_PRE] != 0.0){ //PKT_OUT or flow_mod
 					tm->accumulated[stage] += now - tm->current[TM_SA6_PRE];	//Avoid race condition
+					//fprintf(stderr, "SA6_SUCCESS now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_SA6_PRE]);	
+				}
 				break;
 			case TM_SA6_FAILURE:
 				assert(tm->current[TM_SA6_PRE] != 0.0);
 				tm->accumulated[stage] = now - tm->current[TM_SA6_PRE];
 				tm->total_SA6_dropped_pkts++;
 				tm->total_pkts++;
+				//fprintf(stderr, "SA6_FAILURE now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_SA6_PRE]);	
 				break;
 			case TM_SB6_PRE:
 				assert(tm->current[TM_S5] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_S5];
 				tm->sb6_reached = true;
+				//fprintf(stderr, "SB6_PRE now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_S5]);	
 				break;
 			case TM_SB6_SUCCESS:
 				assert(tm->current[TM_SB6_PRE] != 0.0);
 				tm->accumulated[stage] += now - tm->current[TM_SB6_PRE];
 				tm->total_pktin_pkts++;	
 				tm->total_pkts++;
+				//fprintf(stderr, "SB6_SUCCESS now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_SB6_PRE]);	
 				break;
 			case TM_SB6_FAILURE:
 				assert(tm->current[TM_SB6_PRE] != 0.0);
 				tm->accumulated[stage] = now - tm->current[TM_SB6_PRE];
 				tm->total_SB6_dropped_pkts++;	
 				tm->total_pkts++;
+				//fprintf(stderr, "SB6_FAILURE now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_SB6_PRE]);	
 				break;
 			case TM_SA7:
+				assert(tm->current[TM_SA6_PRE] != 0.0);
 				if(tm->s5_reached && !tm->sb6_reached){ //May be called more than one time
 					assert(tm->current[TM_SA6_PRE] != 0.0);
 					tm->accumulated[stage] += now - tm->current[TM_SA6_PRE];	//Avoid race-condition
+					//fprintf(stderr, "SA7 now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_SA6_PRE]);	
 				}
 				break;
 			case TM_SA8:
+				assert(tm->current[TM_SA7] != 0.0);
 				if(tm->s5_reached && !tm->sb6_reached){ //May be called more than one time
 					assert(tm->current[TM_SA7] != 0.0);
 					tm->accumulated[stage] += now - tm->current[TM_SA7];
 					tm->total_output_pkts++;	
 					tm->total_pkts++;
+					//fprintf(stderr, "SA8 now: %"PRIu64", diff: %"PRIu64"\n", now, now - tm->current[TM_SA7]);	
 				}
 				break;
 			case TM_MAX:

bfcd0134ac32d2f2e8f575d001f978e64d2ec77e
Andreas Köpsel <andreas.koepsel@bisdn.de>
2013-11-16 21:33:10 +0100

    datapacketx86 => added output operator<<() / bufferpool => added static method bufferpool_dump_state()

--- src/xdpd/fwd-modules/gnu_linux/src/io/bufferpool.h
+++ src/xdpd/fwd-modules/gnu_linux/src/io/bufferpool.h
@@ -123,7 +123,7 @@ public:
 		bufferpool& bp = *(bufferpool::get_instance());
 		std::cout << bp << std::endl;
 		for (long long unsigned int i = 0; i < bp.pool_size; i++) {
-			std::cout << *(bp.pool[i]) << std::endl;
+			std::cout << *((datapacketx86*)bp.pool[i]) << std::endl;
 		}
 	};
 };

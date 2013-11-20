4fa861416c8201f5a5708ed2eb68419224a693b0
Victor Alvarez <victor.alvarez@bisdn.de>
2013-11-20 10:48:36 +0100

    Added license to header files

--- src/xdpd/fwd-modules/gnu_linux/src/io/packet_classifiers/c_pktclassifier/headers/cpc_ethernet.h
+++ src/xdpd/fwd-modules/gnu_linux/src/io/packet_classifiers/c_pktclassifier/headers/cpc_ethernet.h
@@ -1,4 +1,6 @@
-//Here: struct definitions, getters, setters (implemented) and declaration of pudh & pop
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef _CPC_ETERNET_H_
 #define _CPC_ETERNET_H_

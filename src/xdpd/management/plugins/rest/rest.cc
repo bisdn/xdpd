e6a9376c2e5beb69f8bdfd258946d78d2392587e
Nick Bastin <nick@bssoftworks.com>
2014-01-13 06:28:42 +0100

    Moved rest endpoints to new file, added stubs for datapaths and ports

--- src/xdpd/management/plugins/rest/rest.cc
+++ src/xdpd/management/plugins/rest/rest.cc
@@ -5,8 +5,6 @@
 #include "rest.h"
 #include <rofl/common/utils/c_logger.h>
 
-#include "../../plugin_manager.h"
-
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/thread.hpp>
@@ -14,36 +12,11 @@
 
 #include "server/server.hpp"
 #include "server/rest_handler.hpp"
-#include "server/request.hpp"
-#include "server/reply.hpp"
 
-#include "json_spirit/json_spirit_writer_template.h"
+#include "endpoints.hpp"
 
 namespace xdpd
   {
-  void list_plugins (const http::server::request &req, http::server::reply &rep)
-    {
-    std::vector<plugin*> plugin_list = plugin_manager::get_plugins();
-
-//    json_spirit::mObject obj;
-
-    std::vector<std::string> plugins;
-
-    for (std::vector<plugin*>::iterator i = plugin_list.begin(); i != plugin_list.end(); ++i)
-      {
-      plugins.push_back((*i)->get_name());
-      }
-
-//    obj["plugins"] = json_spirit::Value(plugins.begin(), plugins.end());
-    json_spirit::Value pa(plugins.begin(), plugins.end());
-
-    rep.content = json_spirit::write_string(pa);
-    }
-
-  void list_ports (const http::server::request &req, http::server::reply &rep)
-    {
-    }
-
   void srvthread ()
     {
     boost::asio::io_service io_service;
@@ -52,7 +25,9 @@ namespace xdpd
       {
       http::server::rest_handler  handler;
 
-      handler.register_path("/plugins", boost::bind(list_plugins, _1, _2));
+      handler.register_path("/plugins", boost::bind(endpoints::list_plugins, _1, _2));
+      handler.register_path("/datapaths", boost::bind(endpoints::list_datapaths, _1, _2));
+      handler.register_path("/ports", boost::bind(endpoints::list_ports, _1, _2));
 
       http::server::server(io_service, "0.0.0.0", "80", handler)();
       boost::asio::signal_set signals(io_service);
@@ -64,14 +39,14 @@ namespace xdpd
       }
     catch(boost::thread_interrupted&)
       {
-      ROFL_INFO("REST Server shutting down");
+      ROFL_INFO("REST Server shutting down\n");
       return;
       }
     }
 
   void rest::init(int args, char** argv)
     {
-    ROFL_INFO("Starting REST server");
+    ROFL_INFO("Starting REST server\n");
     boost::thread t(&srvthread);
 
     return;

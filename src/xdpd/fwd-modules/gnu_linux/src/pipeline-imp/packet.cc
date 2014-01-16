ef492c3831d9629fd391fa531c2d397b0e3fc461
Marc Sune <marc.sune@bisdn.de>
2014-01-16 15:52:35 +0100

    Trying to fix datapacketx86 clonining when X86_DATAPACKET_BUFFERED_IN_NIC is used

--- src/xdpd/fwd-modules/gnu_linux/src/pipeline-imp/packet.cc
+++ src/xdpd/fwd-modules/gnu_linux/src/pipeline-imp/packet.cc
@@ -40,16 +40,16 @@ using namespace xdpd::gnu_linux;
 
 /* Cloning of the packet */
 static void clone_pkt_contents(datapacket_t* src, datapacket_t* dst){
-	
+
 	datapacketx86 *pack_src = (datapacketx86*)src->platform_state;
 	datapacketx86 *pack_dst = (datapacketx86*)dst->platform_state;
-	pack_dst->init(pack_src->get_buffer(), pack_src->get_buffer_length(), pack_src->lsw, pack_src->in_port, pack_src->in_phy_port, true, true);
+	pack_dst->init(pack_src->get_buffer(), pack_src->get_buffer_length(), pack_src->lsw, pack_src->in_port, pack_src->in_phy_port, true, pack_src->get_buffering_status() == X86_DATAPACKET_BUFFERED_IN_USER_SPACE );
 
 	//copy checksum flags
-       pack_dst->ipv4_recalc_checksum = pack_src->ipv4_recalc_checksum;
-       pack_dst->icmpv4_recalc_checksum = pack_src->icmpv4_recalc_checksum;
-       pack_dst->tcp_recalc_checksum = pack_src->tcp_recalc_checksum;
-       pack_dst->udp_recalc_checksum = pack_src->udp_recalc_checksum;
+	pack_dst->ipv4_recalc_checksum = pack_src->ipv4_recalc_checksum;
+	pack_dst->icmpv4_recalc_checksum = pack_src->icmpv4_recalc_checksum;
+	pack_dst->tcp_recalc_checksum = pack_src->tcp_recalc_checksum;
+	pack_dst->udp_recalc_checksum = pack_src->udp_recalc_checksum;
 }
 
 

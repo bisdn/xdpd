be85b7585fa6e07218714232c6a2d6fd98194826
Levent Dane <limon@verj.us>
2014-01-17 14:49:49 +0100

    Use likely/unlikely macros

--- src/xdpd/fwd-modules/gnu_linux/src/io/ports/netmap/ioport_netmap.cc
+++ src/xdpd/fwd-modules/gnu_linux/src/io/ports/netmap/ioport_netmap.cc
@@ -12,6 +12,7 @@ struct netmap_d *ioport_netmap::mem = NULL;
 
 //Constructor and destructor
 ioport_netmap::ioport_netmap(switch_port_t* of_ps, unsigned int num_queues):ioport(of_ps,num_queues){
+	int ret;
 
 	if (!strcmp(of_port_state->name, "lo"))
 		throw "Don't register for loopback";
@@ -20,7 +21,7 @@ ioport_netmap::ioport_netmap(switch_port_t* of_ps, unsigned int num_queues):iopo
 
 	//ROFL_INFO("netmap: %s trying to open with netmap\n", of_port_state->name);
 	fd = open("/dev/netmap", O_RDWR);
-	if (fd == -1) {
+	if (unlikely(fd == -1)) {
 		throw "Check kernel module";
 	}
 
@@ -29,8 +30,8 @@ ioport_netmap::ioport_netmap(switch_port_t* of_ps, unsigned int num_queues):iopo
 	req.nr_version = NETMAP_API;
 
 	strcpy(req.nr_name, of_port_state->name);
-
-	if (ioctl(fd, NIOCREGIF, &req) == -1) {
+	ret = ioctl(fd, NIOCREGIF, &req);
+	if (unlikely(ret == -1)) {
 		close(fd);
 		throw "Unable to register";
 	}
@@ -52,8 +53,8 @@ ioport_netmap::ioport_netmap(switch_port_t* of_ps, unsigned int num_queues):iopo
 
 	//TODO: test
 	num_of_queues=1;
-
-	if(pipe(notify_pipe) == -1)
+	ret = pipe(notify_pipe);
+	if( unlikely(ret == -1) )
 		ROFL_INFO("Pipe problem\n");
 
 	for(unsigned int i=0;i<2;i++) {
@@ -85,7 +86,7 @@ void ioport_netmap::enqueue_packet(datapacket_t* pkt, unsigned int q_id){
 		return;
 	}
 	//Put in the queue
-	if(output_queues[q_id].blocking_write(pkt) != ROFL_SUCCESS ) {
+	if(output_queues[q_id].non_blocking_write(pkt) != ROFL_SUCCESS ) {
 		ROFL_INFO("Queue problem\n");
 		bufferpool::release_buffer(pkt);
 	}
@@ -93,12 +94,14 @@ void ioport_netmap::enqueue_packet(datapacket_t* pkt, unsigned int q_id){
 	ROFL_DEBUG_VERBOSE("[mmap:%s] Packet(%p) enqueued, buffer size: %d\n",  of_port_state->name, pkt, output_queues[q_id].size());
 	//TODO: make it happen only if thread is really sleeping...
 	ret = ::write(notify_pipe[WRITE],&c,sizeof(c));
-	(void)ret;
+	if( unlikely(ret == 0) )
+		ROFL_DEBUG("Can't write into notify_pipe");
 	return;
 }
 
 void ioport_netmap::flush_ring() {
 	struct pollfd x[1];
+	int ret;
 	x[0].fd = fd;
 	x[0].events = (POLLIN);
 
@@ -110,7 +113,8 @@ void ioport_netmap::flush_ring() {
 		ring->cur = NETMAP_RING_NEXT(ring, ring->cur);
 		ring->avail--;
 	}
-	if(ioctl(fd,NIOCTXSYNC|NIOCRXSYNC, NULL) == -1)
+	ret = ioctl(fd,NIOCTXSYNC|NIOCRXSYNC, NULL);
+	if( unlikely(ret == -1) )
 		ROFL_DEBUG_VERBOSE("Netmap sync problem\n");
 	return;
 }
@@ -122,7 +126,7 @@ void ioport_netmap::empty_pipe() {
 
 	ret = ::read(notify_pipe[READ], draining_buffer, deferred_drain);
 
-	if(ret == -1) {
+	if( unlikely(ret == -1) ) {
 		ROFL_INFO("pipe is empty");
 	} else if((deferred_drain - ret) < 0)
 		deferred_drain = 0;
@@ -137,16 +141,10 @@ datapacket_t* ioport_netmap::read(){
 	unsigned int cur;
 	char *buf;
 
-	//epoll ??
-	//struct pollfd x[1];
-	//x[0].fd = fd;
-	//x[0].events = (POLLIN);
-
 	struct netmap_ring *ring;
-	//poll(x, 1, 1000);
 
 	ring = NETMAP_RXRING(nifp, 0);
-	//ROFL_INFO("Ring Avail: %"PRIu32"\n", ring->avail);
+	ROFL_DEBUG("Ring Avail: %"PRIu32"\n", ring->avail);
 
 	if (ring->avail == 0) {
 		//assert(0);
@@ -169,13 +167,13 @@ datapacket_t* ioport_netmap::read(){
 	pkt_x86->init((uint8_t*)buf, slot->len, of_port_state->attached_sw, of_port_state->of_port_num,0,true,false);
 	//pkt_x86->init(buf, slot->len, of_port_state->attached_sw, of_port_state->of_port_num);
 
-	ROFL_INFO("Got pkt %d of size %d ==> %p\n", slot->buf_idx, slot->len, pkt_x86->get_buffer());
+	//ROFL_DEBUG_VERBOSE("Got pkt %d of size %d ==> %p\n", slot->buf_idx, slot->len, pkt_x86->get_buffer());
 
 	ring->cur = NETMAP_RING_NEXT(ring, cur);
 	ring->avail-- ;
 	//ROFL_INFO("We have %zu packets in our ring\n", ring->avail);
 
-	ROFL_DEBUG_VERBOSE("Filled buffer with id:%d. Sending to process.\n", pkt_x86->buffer_id);
+	ROFL_DEBUG_VERBOSE("Filled buffer with id:%d. Sending to process.\n", slot->buf_idx);
 
 //	if(ioctl(fd,NIOCRXSYNC, NULL) == -1)
 //		ROFL_DEBUG_VERBOSE("Netmap sync problem\n");
@@ -248,7 +246,9 @@ unsigned int ioport_netmap::write(unsigned int q_id, unsigned int num_of_buckets
 	ring->cur = cur;
 	empty_pipe();
 	//ROFL_INFO("Sent %d out of %d\n", i,num_of_buckets);
-	if(ioctl(fd,NIOCTXSYNC, NULL) == -1)
+	int ret;
+	ret = ioctl(fd,NIOCTXSYNC, NULL);
+	if(unlikely(ret == -1))
 		ROFL_DEBUG_VERBOSE("Netmap sync problem\n");
 	return num_of_buckets-i;
 }
@@ -300,10 +300,8 @@ rofl_result_t ioport_netmap::enable(){
 		close(sd);
 		return ROFL_FAILURE;
 	}
-	if(IFF_UP | ifr.ifr_flags) {
+	if(IFF_UP & ifr.ifr_flags) {
 		ifr.ifr_flags |= IFF_UP;
-	}
-	if(IFF_PROMISC | ifr.ifr_flags) {
 		ifr.ifr_flags |= IFF_PROMISC;
 	}
 

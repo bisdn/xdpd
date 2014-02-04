9cdbc06c797bcb41b0a646adfcbc326a64600dca
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-02-04 14:17:29 +0100

    of10_translation_utils/of10_endpoint => reverse-mapping uses now cofactions instead of cofinstructions

--- src/xdpd/openflow/openflow10/of10_endpoint.cc
+++ src/xdpd/openflow/openflow10/of10_endpoint.cc
@@ -345,11 +345,8 @@ of10_endpoint::handle_flow_stats_request(
 		cofmatch match(rofl::openflow10::OFP_VERSION);
 		of10_translation_utils::of1x_map_reverse_flow_entry_matches(elem->matches, match);
 
-		cofinstructions instructions(ctl.get_version());
-		of10_translation_utils::of1x_map_reverse_flow_entry_instructions((of1x_instruction_group_t*)(elem->inst_grp), instructions, of10switch->pipeline->miss_send_len);
-
-		if (0 == instructions.size())
-			continue;
+		cofactions actions(rofl::openflow10::OFP_VERSION);
+		of10_translation_utils::of1x_map_reverse_flow_entry_actions((of1x_instruction_group_t*)(elem->inst_grp), actions, of10switch->pipeline->miss_send_len);
 
 		flow_stats.push_back(
 				cofflow_stats_reply(
@@ -364,7 +361,7 @@ of10_endpoint::handle_flow_stats_request(
 						elem->packet_count,
 						elem->byte_count,
 						match,
-						instructions[0].get_actions()));
+						actions));
 		// TODO: check this implicit assumption of always using a single instruction?
 		// this should be an instruction of type OFPIT_APPLY_ACTIONS anyway
 	}

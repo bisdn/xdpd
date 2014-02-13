2f4704122f044f9deaf938641ef9a34dd6a70da7
Marc Sune <marc.sune@bisdn.de>
2014-02-13 16:47:30 +0100

    Refining inlining by symlink mechanism.. Somehow wierd but does the trick

--- src/xdpd/fwd-modules/gnu_linux/src/pipeline-imp/atomic_operations.c
+++ src/xdpd/fwd-modules/gnu_linux/src/pipeline-imp/atomic_operations.c
@@ -1,15 +1,27 @@
-#include <rofl/datapath/pipeline/platform/atomic_operations.h>
+//Guards used only when inlining
+#ifndef ATOMIC_OPS_IMPL_INLINE
+#define ATOMIC_OPS_IMPL_INLINE
+
 
 //Using GCC atomic built-ins when available
 //Defining them as static inline, such that this file can be symlinked 
 //when compiling ROFL-pipeline to inline them
 
 //Uncomment this line when you are inlining them in the pipeline
-//#define STATIC_INLINE__ static inline
-#define STATIC_INLINE__ 
+#define GNU_LINUX_INLINE_ATOMIC 0
+
+#if GNU_LINUX_INLINE_ATOMIC == 1
+	#include <inttypes.h>
+	#define STATIC_ATOMIC_INLINE__ static inline
+#else
+	#define STATIC_ATOMIC_INLINE__ 
+	#include <rofl/datapath/pipeline/platform/atomic_operations.h>
+#endif
+
+#include <rofl/datapath/pipeline/platform/lock.h>
 
 /// these functins increase by one the counter
-STATIC_INLINE__ void platform_atomic_inc64(uint64_t* counter, platform_mutex_t* mutex)
+STATIC_ATOMIC_INLINE__ void platform_atomic_inc64(uint64_t* counter, platform_mutex_t* mutex)
 {
 #if defined(__GNUC__) || defined(__INTEL_COMPILER)
 	__sync_add_and_fetch(counter, 1UL);
@@ -20,7 +32,7 @@ STATIC_INLINE__ void platform_atomic_inc64(uint64_t* counter, platform_mutex_t* 
 #endif	
 }
 
-STATIC_INLINE__ void platform_atomic_inc32(uint32_t* counter, platform_mutex_t* mutex)
+STATIC_ATOMIC_INLINE__ void platform_atomic_inc32(uint32_t* counter, platform_mutex_t* mutex)
 {
 #if defined(__GNUC__) || defined(__INTEL_COMPILER)
 	__sync_add_and_fetch(counter, 1);
@@ -32,7 +44,7 @@ STATIC_INLINE__ void platform_atomic_inc32(uint32_t* counter, platform_mutex_t* 
 }
 
 /// These functions add value to the counter
-STATIC_INLINE__ void platform_atomic_add64(uint64_t* counter, uint64_t* value, platform_mutex_t* mutex)
+STATIC_ATOMIC_INLINE__ void platform_atomic_add64(uint64_t* counter, uint64_t* value, platform_mutex_t* mutex)
 {
 #if defined(__GNUC__) || defined(__INTEL_COMPILER)
 	__sync_add_and_fetch(counter, value);
@@ -43,7 +55,7 @@ STATIC_INLINE__ void platform_atomic_add64(uint64_t* counter, uint64_t* value, p
 #endif
 }
 
-STATIC_INLINE__ void platform_atomic_add32(uint32_t* counter, uint32_t* value, platform_mutex_t* mutex)
+STATIC_ATOMIC_INLINE__ void platform_atomic_add32(uint32_t* counter, uint32_t* value, platform_mutex_t* mutex)
 {
 #if defined(__GNUC__) || defined(__INTEL_COMPILER)
 	__sync_add_and_fetch(counter, value);
@@ -54,7 +66,7 @@ STATIC_INLINE__ void platform_atomic_add32(uint32_t* counter, uint32_t* value, p
 #endif
 }
 
-STATIC_INLINE__ void platform_atomic_dec32(uint32_t* counter, platform_mutex_t* mutex)
+STATIC_ATOMIC_INLINE__ void platform_atomic_dec32(uint32_t* counter, platform_mutex_t* mutex)
 {
 #if defined(__GNUC__) || defined(__INTEL_COMPILER)
 	__sync_sub_and_fetch(counter, 1);
@@ -64,3 +76,5 @@ STATIC_INLINE__ void platform_atomic_dec32(uint32_t* counter, platform_mutex_t* 
 	platform_mutex_unlock(mutex);
 #endif
 }
+
+#endif //Guards used only when inlining
--- src/xdpd/fwd-modules/gnu_linux/src/pipeline-imp/memory.c
+++ src/xdpd/fwd-modules/gnu_linux/src/pipeline-imp/memory.c
@@ -7,36 +7,31 @@
  * malloc and free should be replaced if necessary by platform specific memory allocators
  */
 
-//Uncomment this line when you are inlining them in the pipeline
-//#define STATIC_INLINE__ //static inline
-#define STATIC_INLINE__ 
-
-
 //Per core memory allocators
-STATIC_INLINE__ void* platform_malloc( size_t length ){
+void* platform_malloc( size_t length ){
 	return malloc( length );
 }
-STATIC_INLINE__ void platform_free( void *data ){
+void platform_free( void *data ){
 	free( data );
 }
 
 //Shared memory allocators
-STATIC_INLINE__ void* platform_malloc_shared( size_t length ){
+void* platform_malloc_shared( size_t length ){
 	return malloc( length );
 }
 
-STATIC_INLINE__ void platform_free_shared( void *data ){
+void platform_free_shared( void *data ){
 	free( data );
 }
 
-STATIC_INLINE__ void* platform_memcpy( void *dst, const void *src, size_t length ){
+void* platform_memcpy( void *dst, const void *src, size_t length ){
 	return memcpy( dst, src, length );
 }
 
-STATIC_INLINE__ void* platform_memset( void *src, int c, size_t length ){
+void* platform_memset( void *src, int c, size_t length ){
 	return memset( src, c, length );
 }
 
-STATIC_INLINE__ void* platform_memmove( void *dst, const void *src, size_t length ){
+void* platform_memmove( void *dst, const void *src, size_t length ){
 	return memmove( dst, src, length );
 }

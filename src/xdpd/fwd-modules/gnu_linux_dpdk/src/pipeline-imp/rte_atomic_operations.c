5295ebffbb28e1089afb0a022346c74db0b91a50
Marc Sune <marc.sune@bisdn.de>
2014-02-19 14:17:28 +0100

    Adapting the fwd_module to the new APIs of the pipeline

--- src/xdpd/fwd-modules/gnu_linux_dpdk/src/pipeline-imp/rte_atomic_operations.c
+++ src/xdpd/fwd-modules/gnu_linux_dpdk/src/pipeline-imp/rte_atomic_operations.c
@@ -18,14 +18,14 @@ void platform_atomic_inc32(uint32_t* counter, platform_mutex_t* mutex)
 }
 
 /// These functions add value to the counter
-void platform_atomic_add64(uint64_t* counter, uint64_t* value, platform_mutex_t* mutex)
+void platform_atomic_add64(uint64_t* counter, uint64_t value, platform_mutex_t* mutex)
 {
-	rte_atomic64_add((rte_atomic64_t*)counter, *value);
+	rte_atomic64_add((rte_atomic64_t*)counter, value);
 }
 
-void platform_atomic_add32(uint32_t* counter, uint32_t* value, platform_mutex_t* mutex)
+void platform_atomic_add32(uint32_t* counter, uint32_t value, platform_mutex_t* mutex)
 {
-	rte_atomic32_add((rte_atomic32_t*)counter, *value);
+	rte_atomic32_add((rte_atomic32_t*)counter, value);
 }
 
 void platform_atomic_dec32(uint32_t* counter, platform_mutex_t* mutex)

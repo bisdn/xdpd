1851c37874052f7f6394db73d2db5af5170e7b27
Marc Sune <marc.sune@bisdn.de>
2014-03-27 13:20:01 +0100

    Adapting example driver to the new rofl terminology

--- src/xdpd/drivers/example/src/hal-imp/driver.c
+++ src/xdpd/drivers/example/src/hal-imp/driver.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <net/if.h>
-#include <rofl/datapath/afa/fwd_module.h>
+#include <rofl/datapath/hal/driver.h>
 #include <rofl/common/utils/c_logger.h>
-#include <rofl/datapath/afa/cmm.h>
+#include <rofl/datapath/hal/cmm.h>
 #include <rofl/datapath/pipeline/platform/memory.h>
 #include <rofl/datapath/pipeline/physical_switch.h>
 #include <rofl/datapath/pipeline/openflow/openflow1x/of1x_switch.h>
@@ -13,30 +13,30 @@
 #include <rofl/datapath/pipeline/openflow/of_switch.h>
 #include <rofl/datapath/pipeline/common/datapacket.h>
 
-//Fwd module static info
+//Driver static info
 #define EXAMPLE_CODE_NAME "example"
 #define EXAMPLE_VERSION VERSION 
-#define EXAMPLE_DESC  "Example forwarding module. This is an empty non-functional driver that can be used as a blank canvas to port new platforms."
+#define EXAMPLE_DESC  "Example driver. This is an empty non-functional driver that can be used as a blank canvas to port new platforms."
 #define EXAMPLE_USAGE  "use format: \"key1=value1;key2=value2;\"\n\t\t\t\t\t- key1: explanation of key1\n\t\t\t\t\t- key2: explanation of key2" //Do not terminate it with \n
 
 //Here we are going to store the parameters that we got
 //and that we were able to parse (understand) and execute
-char extra_params[FWD_MOD_EXTRA_PARAMS_MAX_LEN];
+char extra_params[DRIVER_EXTRA_PARAMS_MAX_LEN];
 
 
 /*
-* @name    fwd_module_init
-* @brief   Initializes driver. Before using the AFA_DRIVER routines, higher layers must allow driver to initialize itself
-* @ingroup fwd_module_management
+* @name    driver_init
+* @brief   Initializes driver. Before using the HAL_DRIVER routines, higher layers must allow driver to initialize itself
+* @ingroup driver_management
 */
-afa_result_t fwd_module_init(const char* _extra_params){
+hal_result_t driver_init(const char* _extra_params){
 
 	
-	ROFL_INFO("["FWD_MOD_NAME"] calling fwd_mod_init()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling fwd_mod_init()\n");
 	
 	//If using ROFL-PIPELINE, the physical switch must be inited
 	//if(physical_switch_init() != ROFL_SUCCESS)
-	//	return AFA_FAILURE;
+	//	return HAL_FAILURE;
 
 	//Likely here you are going to discover platform ports;
 	//If using ROFL_pipeline you would then add them (physical_switch_add_port()
@@ -45,33 +45,33 @@ afa_result_t fwd_module_init(const char* _extra_params){
 	
 	//And initialize or setup any other state your platform needs...	
 	
-	ROFL_INFO("["FWD_MOD_NAME"] This forwarding module is an empty example. xDPd will not be able to bootstrap any configuration (LSIs) since it is not a functional forwarding module, so it will throw a deliverately uncaught exception\n");
+	ROFL_INFO("["DRIVER_NAME"] This driver is an empty example. xDPd will not be able to bootstrap any configuration (LSIs) since it is not a functional driver, so it will throw a deliverately uncaught exception\n");
 	
-	strncpy(extra_params, _extra_params, FWD_MOD_EXTRA_PARAMS_MAX_LEN);
-	return AFA_SUCCESS; 
+	strncpy(extra_params, _extra_params, DRIVER_EXTRA_PARAMS_MAX_LEN);
+	return HAL_SUCCESS; 
 }
 
 /**
-* @name    fwd_module_get_info
-* @brief   Get the information of the forwarding_module (code-name, version, usage...)
-* @ingroup fwd_module_management
+* @name    driver_get_info
+* @brief   Get the information of the driver (code-name, version, usage...)
+* @ingroup driver_management
 */
-void fwd_module_get_info(fwd_module_info_t* info){
-	//Fill-in fwd_module_info_t
-	strncpy(info->code_name, EXAMPLE_CODE_NAME, FWD_MOD_CODE_NAME_MAX_LEN);
-	strncpy(info->version, EXAMPLE_VERSION, FWD_MOD_VERSION_MAX_LEN);
-	strncpy(info->description, EXAMPLE_DESC, FWD_MOD_DESCRIPTION_MAX_LEN);
-	strncpy(info->usage, EXAMPLE_USAGE, FWD_MOD_USAGE_MAX_LEN);
-	strncpy(info->extra_params, extra_params, FWD_MOD_EXTRA_PARAMS_MAX_LEN);
+void driver_get_info(driver_info_t* info){
+	//Fill-in driver_info_t
+	strncpy(info->code_name, EXAMPLE_CODE_NAME, DRIVER_CODE_NAME_MAX_LEN);
+	strncpy(info->version, EXAMPLE_VERSION, DRIVER_VERSION_MAX_LEN);
+	strncpy(info->description, EXAMPLE_DESC, DRIVER_DESCRIPTION_MAX_LEN);
+	strncpy(info->usage, EXAMPLE_USAGE, DRIVER_USAGE_MAX_LEN);
+	strncpy(info->extra_params, extra_params, DRIVER_EXTRA_PARAMS_MAX_LEN);
 }
 
 
 /*
-* @name    fwd_module_destroy
+* @name    driver_destroy
 * @brief   Destroy driver state. Allows platform state to be properly released. 
-* @ingroup fwd_module_management
+* @ingroup driver_management
 */
-afa_result_t fwd_module_destroy(){
+hal_result_t driver_destroy(){
 
 	//In this function you allow the platform
 	//to be properly cleaning its own state
@@ -79,15 +79,15 @@ afa_result_t fwd_module_destroy(){
 	//If using the pipeline you should call
 	//physical_switch_destroy();
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling fwd_mod_destroy()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling fwd_mod_destroy()\n");
 	
-	return AFA_SUCCESS; 
+	return HAL_SUCCESS; 
 }
 
 /*
 * Switch management functions
 */
-bool fwd_module_switch_exists(uint64_t dpid){
+bool driver_switch_exists(uint64_t dpid){
 	return NULL;
 }
 
@@ -96,44 +96,44 @@ bool fwd_module_switch_exists(uint64_t dpid){
 * @ingroup logical_switch_management
 * @retval  List of available dpids, which MUST be deleted using dpid_list_destroy().
 */
-dpid_list_t* fwd_module_get_all_lsi_dpids(void){
+dpid_list_t* driver_get_all_lsi_dpids(void){
 	return NULL;
 }
 
 /**
- * @name fwd_module_get_switch_snapshot_by_dpid 
+ * @name driver_get_switch_snapshot_by_dpid 
  * @brief Retrieves a snapshot of the current state of a switch port, if the port name is found. The snapshot MUST be deleted using switch_port_destroy_snapshot()
  * @ingroup logical_switch_management
  * @retval  Pointer to of_switch_snapshot_t instance or NULL 
  */
-of_switch_snapshot_t* fwd_module_get_switch_snapshot_by_dpid(uint64_t dpid){
+of_switch_snapshot_t* driver_get_switch_snapshot_by_dpid(uint64_t dpid){
 	return NULL;
 }
 
 
 /*
-* @name    fwd_module_create_switch 
+* @name    driver_create_switch 
 * @brief   Instruct driver to create an OF logical switch 
 * @ingroup logical_switch_management
 * @retval  Pointer to of_switch_t instance 
 */
-afa_result_t fwd_module_create_switch(char* name, uint64_t dpid, of_version_t of_version, unsigned int num_of_tables, int* ma_list){
+hal_result_t driver_create_switch(char* name, uint64_t dpid, of_version_t of_version, unsigned int num_of_tables, int* ma_list){
 	
-	ROFL_INFO("["FWD_MOD_NAME"] calling create switch. Name: %s, number of tables: %d\n",name, num_of_tables);
+	ROFL_INFO("["DRIVER_NAME"] calling create switch. Name: %s, number of tables: %d\n",name, num_of_tables);
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 /*
-* @name    fwd_module_destroy_switch_by_dpid 
+* @name    driver_destroy_switch_by_dpid 
 * @brief   Instructs the driver to destroy the switch with the specified dpid 
 * @ingroup logical_switch_management
 */
-afa_result_t fwd_module_destroy_switch_by_dpid(const uint64_t dpid){
+hal_result_t driver_destroy_switch_by_dpid(const uint64_t dpid){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling destroy_switch_by_dpid()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling destroy_switch_by_dpid()\n");
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 /*
@@ -144,36 +144,36 @@ afa_result_t fwd_module_destroy_switch_by_dpid(const uint64_t dpid){
 * @brief   Checks if a port with the specified name exists 
 * @ingroup port_management 
 */
-bool fwd_module_port_exists(const char *name){
+bool driver_port_exists(const char *name){
 	return false;
 }
 
 /**
 * @brief   Retrieve the list of names of the available ports of the platform. You may want to 
-* 	   call fwd_module_get_port_snapshot_by_name(name) to get more information of the port 
+* 	   call driver_get_port_snapshot_by_name(name) to get more information of the port 
 * @ingroup port_management
 * @retval  List of available port names, which MUST be deleted using switch_port_name_list_destroy().
 */
-switch_port_name_list_t* fwd_module_get_all_port_names(void){
+switch_port_name_list_t* driver_get_all_port_names(void){
 	return NULL;
 }
 
 /**
- * @name fwd_module_get_port_by_name
+ * @name driver_get_port_by_name
  * @brief Retrieves a snapshot of the current state of a switch port, if the port name is found. The snapshot MUST be deleted using switch_port_destroy_snapshot()
  * @ingroup port_management
  */
-switch_port_snapshot_t* fwd_module_get_port_snapshot_by_name(const char *name){
+switch_port_snapshot_t* driver_get_port_snapshot_by_name(const char *name){
 	return NULL;
 }
 /**
- * @name fwd_module_get_port_by_num
+ * @name driver_get_port_by_num
  * @brief Retrieves a snapshot of the current state of the port of the Logical Switch Instance with dpid at port_num, if exists. The snapshot MUST be deleted using switch_port_destroy_snapshot()
  * @ingroup port_management
  * @param dpid DatapathID 
  * @param port_num Port number
  */
-switch_port_snapshot_t* fwd_module_get_port_snapshot_by_num(uint64_t dpid, unsigned int port_num){
+switch_port_snapshot_t* driver_get_port_snapshot_by_num(uint64_t dpid, unsigned int port_num){
 	
 	of_switch_t* lsw;
 	
@@ -190,7 +190,7 @@ switch_port_snapshot_t* fwd_module_get_port_snapshot_by_num(uint64_t dpid, unsig
 
 
 /*
-* @name    fwd_module_attach_physical_port_to_switch
+* @name    driver_attach_physical_port_to_switch
 * @brief   Attemps to attach a system's port to switch, at of_port_num if defined, otherwise in the first empty OF port number.
 * @ingroup management
 *
@@ -198,56 +198,56 @@ switch_port_snapshot_t* fwd_module_get_port_snapshot_by_num(uint64_t dpid, unsig
 * @param name Port name (system's name)
 * @param of_port_num If *of_port_num is non-zero, try to attach to of_port_num of the logical switch, otherwise try to attach to the first available port and return the result in of_port_num
 */
-afa_result_t fwd_module_attach_port_to_switch(uint64_t dpid, const char* name, unsigned int* of_port_num){
+hal_result_t driver_attach_port_to_switch(uint64_t dpid, const char* name, unsigned int* of_port_num){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling attach_port_to_switch()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling attach_port_to_switch()\n");
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 /**
-* @name    fwd_module_connect_switches
+* @name    driver_connect_switches
 * @brief   Attemps to connect two logical switches via a virtual port. Forwarding module may or may not support this functionality. 
 * @ingroup management
 *
 * @param dpid_lsi1 Datapath ID of the LSI1
 * @param dpid_lsi2 Datapath ID of the LSI2 
 */
-afa_result_t fwd_module_connect_switches(uint64_t dpid_lsi1, switch_port_snapshot_t** port1, uint64_t dpid_lsi2, switch_port_snapshot_t** port2){
+hal_result_t driver_connect_switches(uint64_t dpid_lsi1, switch_port_snapshot_t** port1, uint64_t dpid_lsi2, switch_port_snapshot_t** port2){
 	
-	ROFL_INFO("["FWD_MOD_NAME"] calling connect_switches()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling connect_switches()\n");
 	
-	return AFA_SUCCESS; 
+	return HAL_SUCCESS; 
 }
 
 /*
-* @name    fwd_module_detach_port_from_switch
+* @name    driver_detach_port_from_switch
 * @brief   Detaches a port from the switch 
 * @ingroup port_management
 *
 * @param dpid Datapath ID of the switch to detach the ports
 * @param name Port name (system's name)
 */
-afa_result_t fwd_module_detach_port_from_switch(uint64_t dpid, const char* name){
+hal_result_t driver_detach_port_from_switch(uint64_t dpid, const char* name){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling detach_port_from_switch()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling detach_port_from_switch()\n");
 
-	return AFA_SUCCESS; 
+	return HAL_SUCCESS; 
 }
 
 /*
-* @name    fwd_module_detach_port_from_switch_at_port_num
+* @name    driver_detach_port_from_switch_at_port_num
 * @brief   Detaches port_num of the logical switch identified with dpid 
 * @ingroup port_management
 *
 * @param dpid Datapath ID of the switch to detach the ports
 * @param of_port_num Number of the port (OF number) 
 */
-afa_result_t fwd_module_detach_port_from_switch_at_port_num(uint64_t dpid, const unsigned int of_port_num){
+hal_result_t driver_detach_port_from_switch_at_port_num(uint64_t dpid, const unsigned int of_port_num){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling detach_port_from_switch_at_port_num()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling detach_port_from_switch_at_port_num()\n");
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 
@@ -258,74 +258,74 @@ afa_result_t fwd_module_detach_port_from_switch_at_port_num(uint64_t dpid, const
 */
 
 /*
-* @name    fwd_module_bring_port_up
+* @name    driver_bring_port_up
 * @brief   Brings up a system port. If the port is attached to an OF logical switch, this also schedules port for I/O and triggers PORTMOD message. 
 * @ingroup port_management
 *
 * @param name Port system name 
 */
-afa_result_t fwd_module_bring_port_up(const char* name){
+hal_result_t driver_bring_port_up(const char* name){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling bring_port_up()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling bring_port_up()\n");
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 /*
-* @name    fwd_module_bring_port_down
+* @name    driver_bring_port_down
 * @brief   Shutdowns (brings down) a system port. If the port is attached to an OF logical switch, this also de-schedules port and triggers PORTMOD message. 
 * @ingroup port_management
 *
 * @param name Port system name 
 */
-afa_result_t fwd_module_bring_port_down(const char* name){
+hal_result_t driver_bring_port_down(const char* name){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling bring_port_down()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling bring_port_down()\n");
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 
 /*
-* @name    fwd_module_bring_port_up_by_num
+* @name    driver_bring_port_up_by_num
 * @brief   Brings up a port from an OF logical switch (and the underlying physical interface). This function also triggers the PORTMOD message 
 * @ingroup port_management
 *
 * @param dpid DatapathID 
 * @param port_num OF port number
 */
-afa_result_t fwd_module_bring_port_up_by_num(uint64_t dpid, unsigned int port_num){
+hal_result_t driver_bring_port_up_by_num(uint64_t dpid, unsigned int port_num){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling bring_port_up_by_num()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling bring_port_up_by_num()\n");
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 /*
-* @name    fwd_module_bring_port_down_by_num
+* @name    driver_bring_port_down_by_num
 * @brief   Brings down a port from an OF logical switch (and the underlying physical interface). This also triggers the PORTMOD message.
 * @ingroup port_management
 *
 * @param dpid DatapathID 
 * @param port_num OF port number
 */
-afa_result_t fwd_module_bring_port_down_by_num(uint64_t dpid, unsigned int port_num){
+hal_result_t driver_bring_port_down_by_num(uint64_t dpid, unsigned int port_num){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling bring_port_down_by_num()\n");
+	ROFL_INFO("["DRIVER_NAME"] calling bring_port_down_by_num()\n");
 	
-	return AFA_SUCCESS;
+	return HAL_SUCCESS;
 }
 
 /**
  * @brief Retrieve a snapshot of the monitoring state. If rev is 0, or the current monitoring 
  * has changed (monitoring->rev != rev), a new snapshot of the monitoring state is made. Warning: this 
  * is expensive.
- * @ingroup fwd_module_management
+ * @ingroup driver_management
  *
  * @param rev Last seen revision. Set to 0 to always get a new snapshot 
  * @return A snapshot of the monitoring state that MUST be destroyed using monitoring_destroy_snapshot() or NULL if there have been no changes (same rev)
  */ 
-monitoring_snapshot_state_t* fwd_module_get_monitoring_snapshot(uint64_t rev){
+monitoring_snapshot_state_t* driver_get_monitoring_snapshot(uint64_t rev){
 
 	monitoring_state_t* mon = physical_switch_get_monitoring();
 
@@ -337,16 +337,16 @@ monitoring_snapshot_state_t* fwd_module_get_monitoring_snapshot(uint64_t rev){
 
 /**
  * @brief get a list of available matching algorithms
- * @ingroup fwd_module_management
+ * @ingroup driver_management
  *
  * @param of_version
  * @param name_list
  * @param count
  * @return
  */
-afa_result_t fwd_module_list_matching_algorithms(of_version_t of_version, const char * const** name_list, int *count){
+hal_result_t driver_list_matching_algorithms(of_version_t of_version, const char * const** name_list, int *count){
 
 	if(of_get_switch_matching_algorithms(of_version, name_list, count) == ROFL_SUCCESS)
-		return AFA_SUCCESS;
-	return AFA_FAILURE;
+		return HAL_SUCCESS;
+	return HAL_FAILURE;
 }
--- src/xdpd/drivers/example/src/pipeline-imp/platform_hooks_of1x.cc
+++ src/xdpd/drivers/example/src/pipeline-imp/platform_hooks_of1x.cc
@@ -2,7 +2,7 @@
 #include <rofl/datapath/pipeline/openflow/openflow1x/of1x_async_events_hooks.h>
 #include <rofl/datapath/pipeline/openflow/openflow1x/of1x_switch.h>
 #include <rofl/datapath/pipeline/openflow/openflow1x/pipeline/of1x_flow_table.h>
-#include <rofl/datapath/afa/openflow/openflow1x/of1x_cmm.h>
+#include <rofl/datapath/hal/openflow/openflow1x/of1x_cmm.h>
 #include <rofl/common/utils/c_logger.h>
 
 /*
@@ -10,14 +10,14 @@
 */
 rofl_result_t platform_post_init_of1x_switch(of1x_switch_t* sw){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling %s()\n",__FUNCTION__);
+	ROFL_INFO("["DRIVER_NAME"] calling %s()\n",__FUNCTION__);
 
 	return ROFL_SUCCESS;
 }
 
 rofl_result_t platform_pre_destroy_of1x_switch(of1x_switch_t* sw){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling %s()\n",__FUNCTION__);
+	ROFL_INFO("["DRIVER_NAME"] calling %s()\n",__FUNCTION__);
 	
 	return ROFL_SUCCESS;
 }
@@ -30,7 +30,7 @@ rofl_result_t platform_pre_destroy_of1x_switch(of1x_switch_t* sw){
 */
 void platform_of1x_packet_in(const of1x_switch_t* sw, uint8_t table_id, datapacket_t* pkt, uint16_t send_len, of_packet_in_reason_t reason)
 {
-	ROFL_INFO("["FWD_MOD_NAME"] calling %s()\n",__FUNCTION__);
+	ROFL_INFO("["DRIVER_NAME"] calling %s()\n",__FUNCTION__);
 }
 
 //Flow removed
@@ -38,7 +38,7 @@ void platform_of1x_notify_flow_removed(const of1x_switch_t* sw,
 						of1x_flow_remove_reason_t reason, 
 						of1x_flow_entry_t* removed_flow_entry){
 
-	ROFL_INFO("["FWD_MOD_NAME"] calling %s()\n",__FUNCTION__);
+	ROFL_INFO("["DRIVER_NAME"] calling %s()\n",__FUNCTION__);
 
 }
 

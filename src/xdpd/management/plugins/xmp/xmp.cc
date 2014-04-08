f28a989c23791a26e3cacbf3de80b859d695e520
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-04-08 14:08:57 +0200

    xmp plugin => nicer debug output

--- src/xdpd/management/plugins/xmp/xmp.cc
+++ src/xdpd/management/plugins/xmp/xmp.cc
@@ -37,7 +37,7 @@ xmp::~xmp()
 void
 xmp::init()
 {
-	rofl::logging::error << "[xdpd][xmp] initializing ..." << std::endl;
+	rofl::logging::error << "[xdpd][plugin][xmp] initializing ..." << std::endl;
 	socket->listen(socket_params);
 }
 
@@ -64,17 +64,17 @@ xmp::handle_read(
 
 	if (nbytes == 0) {
 		// socket closed
-		rofl::logging::error << "[xdpd][xmp] reading xmp socket failed, errno:"
+		rofl::logging::error << "[xdpd][plugin][xmp] reading xmp socket failed, errno:"
 				<< errno << " (" << strerror(errno) << ")" << std::endl;
 		return;
 	} else if (nbytes < 0) {
-		rofl::logging::error << "[xdpd][xmp] reading xmp socket failed, errno:"
+		rofl::logging::error << "[xdpd][plugin][xmp] reading xmp socket failed, errno:"
 				<< errno << " (" << strerror(errno) << ")" << std::endl;
 		return;
 	}
 
 	if ((unsigned int)nbytes < sizeof(struct xmp_header_t)) {
-		rofl::logging::error << "[xdpd][xmp] short packet rcvd, rc:" << nbytes << std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] short packet rcvd, rc:" << nbytes << std::endl;
 		return;
 	}
 
@@ -88,7 +88,7 @@ xmp::handle_read(
 	case XMPT_REPLY:
 	case XMPT_NOTIFICATION:
 	default: {
-		rofl::logging::error << "[xdpd][xmp] unknown message rcvd" << std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] unknown message rcvd" << std::endl;
 	};
 	}
 }
@@ -98,10 +98,10 @@ void
 xmp::handle_request(
 		cxmpmsg& msg)
 {
-	rofl::logging::error << "[xdpd][xmp] rcvd message:" << std::endl << msg;
+	rofl::logging::error << "[xdpd][plugin][xmp] rcvd message:" << std::endl << msg;
 
 	if (not msg.get_xmpies().has_ie_command()) {
-		rofl::logging::error << "[xdpd][xmp] rcvd xmp request without -COMMAND- IE, dropping message." << std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp request without -COMMAND- IE, dropping message." << std::endl;
 		return;
 	}
 
@@ -120,7 +120,7 @@ xmp::handle_request(
 	} break;
 	case XMPIEMCT_NONE:
 	default: {
-		rofl::logging::error << "[xdpd][xmp] rcvd xmp request with unknown command:"
+		rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp request with unknown command:"
 				<< (int)msg.get_xmpies().get_ie_command().get_command() << ", dropping message." << std::endl;
 		return;
 	};
@@ -137,12 +137,12 @@ xmp::handle_port_attach(
 
 	try {
 		if (not msg.get_xmpies().has_ie_portname()) {
-			rofl::logging::error << "[xdpd][xmp] rcvd xmp Port-Attach request without -PORTNAME- IE, dropping message." << std::endl;
+			rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp Port-Attach request without -PORTNAME- IE, dropping message." << std::endl;
 			return;
 		}
 
 		if (not msg.get_xmpies().has_ie_dpid()) {
-			rofl::logging::error << "[xdpd][xmp] rcvd xmp Port-Attach request without -DPID- IE, dropping message." << std::endl;
+			rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp Port-Attach request without -DPID- IE, dropping message." << std::endl;
 			return;
 		}
 
@@ -151,20 +151,20 @@ xmp::handle_port_attach(
 
 		unsigned int of_port_num;
 		port_manager::attach_port_to_switch(dpid, portname, &of_port_num);
-		rofl::logging::error << "[xdpd][xmp] attached port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] attached port:" << portname
 				<< " to dpid:" << (unsigned long long)dpid << " "
 				<< " port-no:" << of_port_num << std::endl;
 
 	} catch(eOfSmDoesNotExist& e) {
-		rofl::logging::error << "[xdpd][xmp] attaching port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] attaching port:" << portname
 				<< " to dpid:" << (unsigned long long)dpid << " failed, LSI does not exist" << std::endl;
 
 	} catch(ePmInvalidPort& e) {
-		rofl::logging::error << "[xdpd][xmp] attaching port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] attaching port:" << portname
 				<< " to dpid:" << (unsigned long long)dpid << " failed, port does not exist" << std::endl;
 
 	} catch(eOfSmGeneralError& e) {
-		rofl::logging::error << "[xdpd][xmp] attaching port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] attaching port:" << portname
 				<< " to dpid:" << (unsigned long long)dpid << " failed." << std::endl;
 
 	}
@@ -180,12 +180,12 @@ xmp::handle_port_detach(
 
 	try {
 		if (not msg.get_xmpies().has_ie_portname()) {
-			rofl::logging::error << "[xdpd][xmp] rcvd xmp Port-Detach request without -PORTNAME- IE, dropping message." << std::endl;
+			rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp Port-Detach request without -PORTNAME- IE, dropping message." << std::endl;
 			return;
 		}
 
 		if (not msg.get_xmpies().has_ie_dpid()) {
-			rofl::logging::error << "[xdpd][xmp] rcvd xmp Port-Detach request without -DPID- IE, dropping message." << std::endl;
+			rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp Port-Detach request without -DPID- IE, dropping message." << std::endl;
 			return;
 		}
 
@@ -193,19 +193,19 @@ xmp::handle_port_detach(
 		dpid = msg.get_xmpies().get_ie_dpid().get_dpid();
 
 		port_manager::detach_port_from_switch(dpid, portname);
-		rofl::logging::error << "[xdpd][xmp] detached port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] detached port:" << portname
 				<< " from dpid:" << (unsigned long long)dpid << std::endl;
 
 	} catch(eOfSmDoesNotExist& e) {
-		rofl::logging::error << "[xdpd][xmp] detaching port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] detaching port:" << portname
 				<< " from dpid:" << (unsigned long long)dpid << " failed, LSI does not exist" << std::endl;
 
 	} catch(ePmInvalidPort& e) {
-		rofl::logging::error << "[xdpd][xmp] detaching port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] detaching port:" << portname
 				<< " from dpid:" << (unsigned long long)dpid << " failed, port does not exist" << std::endl;
 
 	} catch(eOfSmGeneralError& e) {
-		rofl::logging::error << "[xdpd][xmp] detaching port:" << portname
+		rofl::logging::error << "[xdpd][plugin][xmp] detaching port:" << portname
 				<< " from dpid:" << (unsigned long long)dpid << " failed." << std::endl;
 
 	}
@@ -220,20 +220,20 @@ xmp::handle_port_enable(
 
 	try {
 		if (not msg.get_xmpies().has_ie_portname()) {
-			rofl::logging::error << "[xdpd][xmp] rcvd xmp Port-Bring-Up request without -PORTNAME- IE, dropping message." << std::endl;
+			rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp Port-Bring-Up request without -PORTNAME- IE, dropping message." << std::endl;
 			return;
 		}
 
 		portname = msg.get_xmpies().get_ie_portname().get_portname();
 
 		port_manager::bring_up(portname);
-		rofl::logging::error << "[xdpd][xmp] brought port:" << portname <<" up"<< std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] brought port:" << portname <<" up"<< std::endl;
 
 	} catch(ePmInvalidPort& e) {
-		rofl::logging::error << "[xdpd][xmp] bringing port:" << portname << " up failed, port does not exist" << std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] bringing port:" << portname << " up failed, port does not exist" << std::endl;
 
 	} catch(eOfSmGeneralError& e) {
-		rofl::logging::error << "[xdpd][xmp] bringing port:" << portname << " up failed." << std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] bringing port:" << portname << " up failed." << std::endl;
 
 	}
 }
@@ -247,20 +247,20 @@ xmp::handle_port_disable(
 
 	try {
 		if (not msg.get_xmpies().has_ie_portname()) {
-			rofl::logging::error << "[xdpd][xmp] rcvd xmp Port-Bring-Down request without -PORTNAME- IE, dropping message." << std::endl;
+			rofl::logging::error << "[xdpd][plugin][xmp] rcvd xmp Port-Bring-Down request without -PORTNAME- IE, dropping message." << std::endl;
 			return;
 		}
 
 		portname = msg.get_xmpies().get_ie_portname().get_portname();
 
 		port_manager::bring_down(portname);
-		rofl::logging::error << "[xdpd][xmp] brought port:" << portname <<" down"<< std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] brought port:" << portname <<" down"<< std::endl;
 
 	} catch(ePmInvalidPort& e) {
-		rofl::logging::error << "[xdpd][xmp] bringing port:" << portname << " down failed, port does not exist" << std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] bringing port:" << portname << " down failed, port does not exist" << std::endl;
 
 	} catch(eOfSmGeneralError& e) {
-		rofl::logging::error << "[xdpd][xmp] bringing port:" << portname << " down failed." << std::endl;
+		rofl::logging::error << "[xdpd][plugin][xmp] bringing port:" << portname << " down failed." << std::endl;
 
 	}
 }

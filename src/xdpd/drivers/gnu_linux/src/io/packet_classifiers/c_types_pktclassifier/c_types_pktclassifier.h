26a9755be01bbe7feb3b73a62c071e6afa619455
Marc Sune <marc.sune@bisdn.de>
2014-04-29 19:52:43 +0200

    Adding 802.3 and EthernetII unroll. Adding first skeleton of the transitions matrix

--- src/xdpd/drivers/gnu_linux/src/io/packet_classifiers/c_types_pktclassifier/c_types_pktclassifier.h
+++ src/xdpd/drivers/gnu_linux/src/io/packet_classifiers/c_types_pktclassifier/c_types_pktclassifier.h
@@ -59,7 +59,9 @@ ROFL_BEGIN_DECLS
 static inline 
 void* get_ether_hdr(classify_state_t* clas_state, int idx){
 	uint8_t* tmp;
-	PKT_TYPES_GET_HDR(tmp, clas_state, PT_PROTO_ETH); 
+	PKT_TYPES_GET_HDR(tmp, clas_state, PT_PROTO_ETHERNET);
+	if(!tmp)
+		PKT_TYPES_GET_HDR(tmp, clas_state, PT_PROTO_8023);
 	return tmp; 
 }
 
@@ -127,7 +129,7 @@ void* get_icmpv6_opt_lladr_source_hdr(classify_state_t* clas_state, int idx){		u
 
 static inline
 void* get_icmpv6_opt_lladr_target_hdr(classify_state_t* clas_state, int idx){
-		uint8_t* tmp;
+	uint8_t* tmp;
 	PKT_TYPES_GET_HDR(tmp, clas_state, PT_PROTO_ICMPV6_OPTS_LLADR_TGT); 
 	return tmp; 
 }
@@ -177,8 +179,203 @@ void* get_gtpu_hdr(classify_state_t* clas_state, int idx){
 //
 // Parsing code
 //
-//XXX FIXME
 
+static inline
+void parse_ipv4(classify_state_t* clas_state, uint8_t *data, size_t datalen){
+
+	unsigned header_len_bytes;	
+
+	//Set reference
+	cpc_ipv4_hdr_t *ipv4 = (cpc_ipv4_hdr_t*)data; 
+
+	//Set frame
+	header_len_bytes = ipv4->ihlvers*4;
+	data += header_len_bytes;
+	datalen -=  header_len_bytes; 
+
+
+	switch (get_ipv4_proto(ipv4)) {
+		case IPV4_IP_PROTO:
+			{
+				//parse_ipv4(clas_state, data, datalen);
+			}
+			break;
+		case ICMPV4_IP_PROTO:
+			{
+				//parse_icmpv4(clas_state, data, datalen);
+			}
+			break;
+		case UDP_IP_PROTO:
+			{
+				//parse_udp(clas_state, data, datalen);
+			}
+			break;
+		case TCP_IP_PROTO:
+			{
+				//parse_tcp(clas_state, data, datalen);
+			}
+			break;
+#if 0
+		case SCTP_IP_PROTO:
+			{
+				parse_sctp(clas_state, data, datalen);
+			}
+			break;
+#endif
+		default:
+			{
+			
+			}
+			break;
+	}
+
+}
+
+
+static inline
+void parse_mpls(classify_state_t* clas_state, uint8_t* data, size_t datalen)
+{
+	
+	//Determine the number of MPLS stacked tags
+	unsigned int n_labels=0;	
+	cpc_mpls_hdr_t* mpls;
+
+	do{
+		n_labels++;
+		data += sizeof(cpc_mpls_hdr_t);
+		datalen -= sizeof(cpc_mpls_hdr_t);
+		mpls = (cpc_mpls_hdr_t*)data;
+	}while(! get_mpls_bos(mpls));
+
+	//Supported types; MPLS over ethernet, MPLS over VLAN
+	switch (clas_state->type){
+		case PT_ETHERNET:
+				PT_ASSIGN_MPLS_TYPE(clas_state->type, PT_ETHERNET, n_labels);
+				break;
+		case PT_ETHERNET_VLAN:
+				PT_ASSIGN_MPLS_TYPE(clas_state->type, PT_ETHERNET_VLAN, n_labels);
+				break;
+		default:
+			assert(0);
+			return;
+	}
+}
+
+static inline
+void parse_vlan(classify_state_t* clas_state, uint8_t *data, size_t datalen){
+
+	//Data pointer	
+	cpc_vlan_hdr_t* vlan;
+	unsigned int n_vlans=0;	
+	uint16_t eth_type;
+	n_vlans=0;
+
+	//Set frame
+	do{
+		vlan = (cpc_vlan_hdr_t *)data;
+		eth_type = get_vlan_type(vlan);
+		data += sizeof(cpc_vlan_hdr_t);
+		datalen -= sizeof(cpc_vlan_hdr_t);
+		n_vlans++;
+	}while(
+		eth_type == VLAN_CTAG_ETHER_TYPE || 
+		eth_type == VLAN_STAG_ETHER_TYPE ||
+		eth_type == VLAN_ITAG_ETHER_TYPE 
+	);
+
+	//Assign type
+	//FIXME: use n_vlans 
+	clas_state->type = PT_ETHERNET_VLAN;	
+	
+	switch (eth_type) {
+		case ETH_TYPE_MPLS_UNICAST:
+		case ETH_TYPE_MPLS_MULTICAST:
+			{
+				parse_mpls(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_PPPOE_DISCOVERY:
+		case ETH_TYPE_PPPOE_SESSION:
+			{
+				//parse_pppoe(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_ARP:
+			{
+				//parse_arpv4(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_IPV4:
+			{
+				//parse_ipv4(clas_state, data, datalen);
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+static inline 
+void parse_ethernet(classify_state_t* clas_state, uint8_t* data, size_t datalen){
+
+	//Data pointer	
+	cpc_eth_hdr_t* ether = (cpc_eth_hdr_t *)data;
+
+	//Set frame
+	if( is_llc_frame(ether) ){
+		data += sizeof(cpc_eth_llc_hdr_t);
+		datalen -= sizeof(cpc_eth_llc_hdr_t);
+		clas_state->type = PT_8023;
+	}else{
+		data += sizeof(cpc_eth_hdr_t);
+		datalen -= sizeof(cpc_eth_hdr_t);
+		clas_state->type = PT_ETHERNET;
+	}
+
+	switch ( get_ether_type(ether)) {
+		case VLAN_CTAG_ETHER_TYPE:
+		case VLAN_STAG_ETHER_TYPE:
+		case VLAN_ITAG_ETHER_TYPE:
+			{
+				//parse_vlan(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_MPLS_UNICAST:
+		case ETH_TYPE_MPLS_MULTICAST:
+			{
+				parse_mpls(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_PPPOE_DISCOVERY:
+		case ETH_TYPE_PPPOE_SESSION:
+			{
+				//parse_pppoe(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_ARP:
+			{
+				//parse_arpv4(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_IPV4:
+			{
+				parse_ipv4(clas_state, data, datalen);
+			}
+			break;
+		case ETH_TYPE_IPV6:
+			{
+				//parse_ipv6(clas_state, data,datalen);
+			}
+			break;
+		default:
+			{
+				
+			}
+			break;
+	}
+
+
+}
 
 //
 // Main functions
--- src/xdpd/drivers/gnu_linux/src/io/packet_classifiers/c_types_pktclassifier/tools/pkt_types_gen.py
+++ src/xdpd/drivers/gnu_linux/src/io/packet_classifiers/c_types_pktclassifier/tools/pkt_types_gen.py
@@ -7,7 +7,8 @@ import re
 #protocols = OrderedDict = 
 protocols = OrderedDict(
 	(
-	("ETH", 14), 
+	("ETHERNET", 14), 
+	("8023", 17), 
 	("MPLS", 4), 
 	("VLAN", 4), 
 	("PPPOE", 20), 
@@ -32,7 +33,7 @@ protocols = OrderedDict(
 # Packet types (using a notation similar to scapy)
 #
 pkt_types = [ 
-	"ETH",
+	"ETH\VLAN",
 
 	#MPLS - no parsing beyond 	
 	"ETH\VLAN/MPLS",
@@ -84,20 +85,24 @@ def filter_pkt_type(pkt_type):
 
 def unroll_pkt_types():
 	for type__ in pkt_types:
+
 		#Unroll ETH or ETH + VLAN
 		unrolled_types=[]
 		if "ETH\\" in type__:
-			#Add with optional
-			tmp = type__.replace("ETH\\","ETH/")
+			#Add ETHERNET+VLAN 
+			tmp = type__.replace("ETH\\","ETHERNET/")
 			unrolled_types.append(tmp)
-			
+			#Add 802.3+VLAN
+			tmp = type__.replace("ETH\\","8023/")
+			unrolled_types.append(tmp)
+	
 			#Add without optional
-			#re.sub('(.?)\\\w+/(.?)','\1/\2', type__)
-			#unrolled_types.append(type__)
-			tmp2 = type__.replace("ETH\\VLAN","ETH")
+			tmp2 = type__.replace("ETH\\VLAN","ETHERNET")
 			unrolled_types.append(tmp2)
-			#print "Generating %s %s "% (tmp, type__)
-			#print "Generating %s %s "% (tmp, tmp2)
+			#Add 802.3
+			tmp2 = type__.replace("ETH\\VLAN","8023")
+			unrolled_types.append(tmp2)
+			
 		else:
 			unrolled_types.append(type__)
 			
@@ -203,8 +208,73 @@ def packet_offsets(f):
 		
 	f.write("\n};\n\n")
 	
+def parse_transitions(f):
+	
+	# Unroll protocols (only IPv4)
+	#
+	# We need to transition based on IPv4 options
+	#
+	unrolled_protocols = OrderedDict()
+	
+	for proto in protocols:
+		if "IPV4" in proto:
+			for i in range(0,ipv4_max_options_words+1):
+				unrolled_protocols[proto.replace("IPV4","IPV4_noptions_"+str(i))] = 0
+			continue
+		unrolled_protocols[proto] = 0
+	
+	f.write("const int parse_transitions [PT_MAX__]["+str(unrolled_protocols.__len__())+"] = {")
+
+	#Comment to help identifying the protocols
+	f.write("\n\t/* {")
+	for type_ in unrolled_protocols:
+		f.write(type_+",")
+	f.write("}*/ \n")
+
+	#Real rows
+	first_type = True 
+	for type_ in pkt_types_unrolled:
+		
+		if not first_type:
+			f.write(",")
+		first_type = False
+
+		len=0
+		row=[]
+		for proto in unrolled_protocols:
+			if "MPLS" in proto:
+				#Special cases: MPLS
+				pass #XXX				
+			else:
+				new_type=type_+"/"+proto
+
+			if new_type in pkt_types_unrolled:
+				row.append("PT_"+sanitize_pkt_type(new_type))
+			else:
+				row.append("0")
+			#row.append("PT_"+sanitize_pkt_type(type_))
+		f.write("\n\t/* "+type_+" */ {")
+
+		first_proto = True
+		for next_proto in row: 
+			if not first_proto:
+				f.write(",")
+			first_proto = False
+			f.write(next_proto)
+
+		f.write("}")
+		
+	f.write("\n};\n\n")
+
 def get_hdr_macro(f):
 	f.write("\n#define PKT_TYPES_GET_HDR(tmp, state, proto)\\\n\tdo{\\\n\t\ttmp = state->base + protocol_offsets_bt[ state->type ][ proto ];\\\n\t\tif(tmp < state->base )\\\n\t\t\ttmp = NULL;\\\n\t}while(0)\n\n")
+
+def mpls_assign_type_macro(f):
+	f.write("\n#define PT_ASSIGN_MPLS_TYPE(t, PREFIX, n_labels) do{\\\n\tswitch(n_labels){\\\n")
+	for i in range(1,mpls_max_depth+1):
+		f.write("\t\tcase "+str(i)+": t = PREFIX##_MPLS_nlabels_##"+str(i)+";\\\n\t\tbreak;\\\n")
+	#f.write("\t\tdefault: {assert(0);} break;\\\n")
+	f.write("}}while(0)\n")
 
 ##
 ## Main function
@@ -235,8 +305,12 @@ def main():
 		#Offsets in bytes
 		packet_offsets(f)
 
+		#Transitions
+		parse_transitions(f)
+
 		#Macros
 		get_hdr_macro(f)
+		mpls_assign_type_macro(f)
 	
 		#End of guards
 		end_guard(f)

f3bcc79a7641b85809ca8ed54c9757e1b3cec068
Marc Sune <marc.sune@bisdn.de>
2014-05-23 11:15:31 +0200

    Adding PIRL's empty skeleton in the build system. fixes #13

---
+++ src/xdpd/drivers/gnu_linux/src/io/pirl/pirl.h
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef PIRL_H
+#define PIRL_H 
+
+/**
+* @file pirl.h
+* @author Marc Sune<marc.sune (at) bisdn.de>
+*
+* @brief Packet-In Rate Limiter (PIRL)
+*/
+
+#include <stdlib.h>
+#include <rofl/datapath/pipeline/common/packet_matches.h>
+
+#include "pirl_config.h"
+
+//FIXME
+
+//C++ extern C
+ROFL_BEGIN_DECLS
+
+//FIXME
+
+//C++ extern C
+ROFL_END_DECLS
+
+#endif //PIRL
---
+++ src/xdpd/drivers/gnu_linux/src/io/pirl/pirl_config.h
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef PIRL_CONFIG_H
+#define PIRL_CONFIG_H 
+
+/**
+* @file pirl_config.h
+* @author Marc Sune<marc.sune (at) bisdn.de>
+*
+* @brief PIRL settings 
+*
+* The only reason for this file to exist is to be able to reuse pirl.h/pirl.c without
+* any change
+*
+*/
+
+//FIXME: add 
+
+#endif //PIRL_CONFIG

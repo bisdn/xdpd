dfcdc82b5ab46d39bab153161a63e700c601c1e7
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-05-28 13:44:44 +0200

    of13_endpoint => added handler stub for meter-mod messages

--- src/xdpd/openflow/openflow13/of13_endpoint.cc
+++ src/xdpd/openflow/openflow13/of13_endpoint.cc
@@ -1542,6 +1542,17 @@ of13_endpoint::handle_experimenter_message(
 
 
 void
+of13_endpoint::handle_meter_mod(
+		rofl::crofctl& ctl,
+		const cauxid& auxid,
+		rofl::openflow::cofmsg_meter_mod& msg)
+{
+	// TODO
+}
+
+
+
+void
 of13_endpoint::handle_ctl_attached(crofctl *ctrl)
 {
 	ROFL_INFO("[sw: %s]Controller %s:%u is in CONNECTED state. \n", sw->dpname.c_str() , ctrl->get_peer_addr().c_str()); //FIXME: add role

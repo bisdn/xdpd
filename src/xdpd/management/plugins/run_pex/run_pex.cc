edb671516f60304a91f9bc2467c0146f6a964469
Ivano Cerrato <ivano.cerrato@polito.it>
2014-07-11 11:44:05 +0200

    Small changes in the run pex plugin

--- src/xdpd/management/plugins/run_pex/run_pex.cc
+++ src/xdpd/management/plugins/run_pex/run_pex.cc
@@ -21,7 +21,7 @@ void runPEX::init()
 	ROFL_INFO("[xdpd]["PLUGIN_NAME"] This plugin instantiates some PEX, whose description is embedded into the code.\n");
 	ROFL_INFO("[xdpd]["PLUGIN_NAME"] **************************\n\n");	
 	
-	std::string pexName1 = "vEth1";
+	/*std::string pexName1 = "vEth1";
 	std::string pexName2 = "vEth2";
 	std::string pexName3 = "vEth3";
 	std::string pexName4 = "vEth4";
@@ -30,23 +30,28 @@ void runPEX::init()
 	std::string pexName7 = "vEth7";
 	std::string pexName8 = "vEth8";
 	std::string pexName9 = "vEth9";
-	std::string pexName10 = "vEth10";
+	std::string pexName10 = "vEth10";*/
 	
-	std::string scriptPath = "/home/ivano/Desktop/pex";
-	
+	std::string pexName1 = "2_example";
+	std::string port1 = "2_example_1";
+	std::string port2 = "2_example_2";
+		
 	//Create two PEX
 	try
 	{
-		pex_manager::create_pex_port(pexName1,pexName1,DOCKER);
-		pex_manager::create_pex_port(pexName2,pexName2,DOCKER);
-		pex_manager::create_pex_port(pexName3,pexName3,DOCKER);
-		pex_manager::create_pex_port(pexName4,pexName4,DOCKER);
-		pex_manager::create_pex_port(pexName5,pexName5,DOCKER);
-		pex_manager::create_pex_port(pexName6,pexName6,DOCKER);
-		pex_manager::create_pex_port(pexName7,pexName7,DOCKER);
-		pex_manager::create_pex_port(pexName8,pexName8,DOCKER);
-		pex_manager::create_pex_port(pexName9,pexName9,DOCKER);
-		pex_manager::create_pex_port(pexName10,pexName10,DOCKER);
+		pex_manager::create_pex_port(pexName1,port1,DPDK_SECONDARY);
+		pex_manager::create_pex_port(pexName1,port2,DPDK_SECONDARY);
+	
+/*		pex_manager::create_pex_port(pexName1,pexName1,DPDK_KNI);
+		pex_manager::create_pex_port(pexName2,pexName2,DPDK_KNI);
+		pex_manager::create_pex_port(pexName3,pexName3,DPDK_KNI);
+		pex_manager::create_pex_port(pexName4,pexName4,DPDK_KNI);
+		pex_manager::create_pex_port(pexName5,pexName5,DPDK_KNI);
+		pex_manager::create_pex_port(pexName6,pexName6,DPDK_KNI);
+		pex_manager::create_pex_port(pexName7,pexName7,DPDK_KNI);
+		pex_manager::create_pex_port(pexName8,pexName8,DPDK_KNI);
+		pex_manager::create_pex_port(pexName9,pexName9,DPDK_KNI);
+		pex_manager::create_pex_port(pexName10,pexName10,DPDK_KNI);*/
 	}catch(...)
 	{
 		ROFL_ERR("[xdpd]["PLUGIN_NAME" Unable to create a port");
@@ -83,9 +88,12 @@ void runPEX::init()
 			{
 				//Attach
 				unsigned int port_number = 0;
-				ROFL_INFO("[xdpd]["PLUGIN_NAME"] Attaching PEX port '%s' to LSI '%x'\n",pexName1.c_str(),dpid);	
-				port_manager::attach_port_to_switch(dpid, pexName1, &port_number);
+				ROFL_INFO("[xdpd]["PLUGIN_NAME"] Attaching PEX port '%s' to LSI '%x'\n",port1.c_str(),dpid);	
+				port_manager::attach_port_to_switch(dpid, port1, &port_number);
 				port_number = 0;
+				ROFL_INFO("[xdpd]["PLUGIN_NAME"] Attaching PEX port '%s' to LSI '%x'\n",port2.c_str(),dpid);	
+				port_manager::attach_port_to_switch(dpid, port2, &port_number);
+/*				port_number = 0;
 				ROFL_INFO("[xdpd]["PLUGIN_NAME"] Attaching PEX port '%s' to LSI '%x'\n",pexName2.c_str(),dpid);	
 				port_manager::attach_port_to_switch(dpid, pexName2, &port_number);
 				port_number = 0;
@@ -111,15 +119,16 @@ void runPEX::init()
 				port_manager::attach_port_to_switch(dpid, pexName9, &port_number);
 				port_number = 0;
 				ROFL_INFO("[xdpd]["PLUGIN_NAME"] Attaching PEX port '%s' to LSI '%x'\n",pexName10.c_str(),dpid);	
-				port_manager::attach_port_to_switch(dpid, pexName10, &port_number);
+				port_manager::attach_port_to_switch(dpid, pexName10, &port_number);*/
 			}catch(...)
 			{	
 				ROFL_ERR("[xdpd]["PLUGIN_NAME"] Unable to attach port to LSI '%s'. Unknown error.\n",dpid);
 				return;
 			}
 			//Bring up
-			port_manager::bring_up(pexName1);
-			port_manager::bring_up(pexName2);
+			port_manager::bring_up(port1);
+			port_manager::bring_up(port2);
+/*			port_manager::bring_up(pexName2);
 			port_manager::bring_up(pexName3);
 			port_manager::bring_up(pexName4);
 			port_manager::bring_up(pexName5);
@@ -128,22 +137,9 @@ void runPEX::init()
 			port_manager::bring_up(pexName8);
 			port_manager::bring_up(pexName9);
 			port_manager::bring_up(pexName10);
+*/
 
-//			*try
-//			{
-				//Attach
-//				unsigned int port_number = 0;
-//				ROFL_INFO("[xdpd]["PLUGIN_NAME"] Attaching PEX port '%s' to LSI '%x'\n",pexName2.c_str(),dpid);	
-//				port_manager::attach_port_to_switch(dpid, pexName2, &port_number);
-//			}catch(...)
-//			{	
-//				ROFL_ERR("[xdpd]["PLUGIN_NAME"] Unable to attach port '%s' to LSI '%s'. Unknown error.\n", pexName2.c_str(),dpid);
-//				return;
-//			}
-			//Bring up
-//			port_manager::bring_up(pexName2);
-	
-//			ROFL_INFO("[xdpd]["PLUGIN_NAME"] All the PEX have been created, and connected to an LSI.\n\n");		
+			ROFL_INFO("[xdpd]["PLUGIN_NAME"] All the PEX have been created, and connected to an LSI.\n\n");		
 
 		}
 	}

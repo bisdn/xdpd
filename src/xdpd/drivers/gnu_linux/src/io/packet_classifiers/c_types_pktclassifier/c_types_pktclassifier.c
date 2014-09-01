b1a62d961e3b72a68b7493b1d10614c5848c70a0
Andreas Koepsel <andreas.koepsel@bisdn.de>
2014-09-01 11:32:49 +0200

    c_types_pktclassifier => function push_gtp => removing re-classification used in tests, as it is not necessary here normally

--- src/xdpd/drivers/gnu_linux/src/io/packet_classifiers/c_types_pktclassifier/c_types_pktclassifier.c
+++ src/xdpd/drivers/gnu_linux/src/io/packet_classifiers/c_types_pktclassifier/c_types_pktclassifier.c
@@ -472,9 +472,7 @@ void* push_gtp(datapacket_t* pkt, classifier_state_t* clas_state, uint16_t ether
 	//set_gtpu_seq_no(gtp_header, 0);
 	//set_gtpu_npdu_no(gtp_header, 0);
 
-	//reclassify
-	//classify_packet(clas_state, clas_state->base, clas_state->len, clas_state->port_in, clas_state->phy_port_in);
-	parse_ethernet(clas_state, clas_state->base, clas_state->len);
+	// do not re-classify, unnecessary
 
 	return NULL;
 }

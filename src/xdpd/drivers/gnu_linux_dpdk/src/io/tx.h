bisdn/xdpd

commit aaea3099df49ddfed2012d84be297773c1dab77f

Fixing shmem TX caching

src/xdpd/drivers/gnu_linux_dpdk/src/io/tx.h
@@ -165,24 +165,40 @@ tx_pkt(switch_port_t* port, unsigned int queue_id, datapacket_t* pkt){
 /**
 * Shmem port
 */
-inline void
-tx_pkt_shmem_nf_port(switch_port_t* port, datapacket_t* pkt)
-{
-	assert(port->type == PORT_TYPE_NF_SHMEM);
+void inline
+flush_shmem_nf_port(switch_port_t* port, rte_ring* queue, struct mbuf_burst* burst){
 
-	int ret;
+	unsigned ret;
 #ifdef ENABLE_DPDK_SECONDARY_SEMAPHORE
 	uint32_t tmp, next_tmp;
 #endif
-	dpdk_shmem_port_state *port_state = (dpdk_shmem_port_state_t*)port->platform_port_state;
-	struct rte_mbuf* mbuf;
+	if( burst->len == 0 || unlikely((port->up == false)) ){
+		return;
+	}
 
-	//Get mbuf pointer
-	mbuf = ((datapacket_dpdk_t*)pkt->platform_state)->mbuf;
+	ROFL_DEBUG_VERBOSE(DRIVER_NAME"[io][shmem][%s] Trying to flush burst(enqueue in lcore ring) of length: %u\n", port->name,  burst->len);
 
-	ret = rte_ring_mp_enqueue(port_state->to_nf_queue, (void *) mbuf);
-	if( likely((ret == 0) || (ret == -EDQUOT)) ){
+	//Enqueue to the hsmem ring
+	ret = rte_ring_mp_enqueue_burst(queue, (void **)burst->burst, burst->len);
+
+	ROFL_DEBUG_VERBOSE(DRIVER_NAME"[io][shmem][%s] --- Flushed %u pkts\n", port->name, ret);
+
 #ifdef ENABLE_DPDK_SECONDARY_SEMAPHORE
+	unsigned int ret_cpy = ret;
+#endif
+
+	if (unlikely(ret < burst->len)) {
+		//TODO increase error counters?
+		do {
+			rte_pktmbuf_free(burst->burst[ret]);
+		} while (++ret < burst->len);
+	}
+
+#ifdef ENABLE_DPDK_SECONDARY_SEMAPHORE
+	dpdk_shmem_port_state *port_state = (dpdk_shmem_port_state_t*)port->platform_port_state;
+	if( likely(ret_cpy >0)){
+		unsigned int i;
+
 		//The packet has been enqueued
 
 		//XXX port_statistics[port].tx++;
@@ -191,38 +207,58 @@ tx_pkt_shmem_nf_port(switch_port_t* port, datapacket_t* pkt)
 		//from the last sem_post
 		do{
 			tmp = port_state->counter_from_last_flush;
-			if(tmp == (PKT_TO_NF_THRESHOLD - 1))
-				//Reset the counter
-				next_tmp = 0;
-			else
-				next_tmp = tmp + 1;
+			next_tmp = (tmp + ret_cpy) % PKT_TO_NF_THRESHOLD;
 		}while(__sync_bool_compare_and_swap(&(port_state->counter_from_last_flush),tmp,next_tmp) == false);
 
-		if(next_tmp == 0){
-			//Notify that pkts are available
+		//Notify that pkts are available
+		for(i=0;i<ret_cpy;++i)
 			sem_post(port_state->semaphore);
-		}
-#endif
-	}else{
-			//The queue is full, and the pkt must be dropped
-
-			//XXX port_statistics[port].dropped++
-
-			rte_pktmbuf_free(mbuf);
 	}
+#endif
+
+	//Reset queue size
+	burst->len = 0;
 }
 
-void inline
-flush_shmem_nf_port(switch_port_t *port)
+inline void
+tx_pkt_shmem_nf_port(switch_port_t* port, datapacket_t* pkt)
 {
-	assert(port != NULL);
-	dpdk_shmem_port_state *port_state = (dpdk_shmem_port_state_t*)port->platform_port_state;
+	struct mbuf_burst* pkt_burst;
+	unsigned int len;
 
-	(void)port_state;
-#ifdef ENABLE_DPDK_SECONDARY_SEMAPHORE
-	sem_post(port_state->semaphore);
+	dpdk_shmem_port_state *port_state = (dpdk_shmem_port_state_t*)port->platform_port_state;
+	struct rte_mbuf* mbuf;
+
+	//Get mbuf pointer
+	mbuf = ((datapacket_dpdk_t*)pkt->platform_state)->mbuf;
+
+	//Recover core task
+	core_tasks_t* tasks = &processing_core_tasks[rte_lcore_id()];
+
+	//Recover burst container (cache)
+	pkt_burst = &tasks->nf_ports[port_state->nf_id].tx_queues_burst[0];
+
+#if DEBUG
+	if(unlikely(!pkt_burst)){
+		rte_pktmbuf_free(mbuf);
+		assert(0);
+		return;
+	}
 #endif
 
+	ROFL_DEBUG_VERBOSE(DRIVER_NAME"[io][shmem] Adding packet %p to queue %p (id: %u)\n", pkt, pkt_burst, rte_lcore_id());
+
+	//Enqueue
+	len = pkt_burst->len;
+	pkt_burst->burst[len] = mbuf;
+	len++;
+
+	//If burst is full => trigger send
+	if ( unlikely(!tasks->active) || unlikely(len == IO_IFACE_MAX_PKT_BURST)) { //If buffer is full or mgmt core
+		pkt_burst->len = len;
+		flush_shmem_nf_port(port, port_state->to_nf_queue, pkt_burst);
+		return;
+	}
 }
 
 /**

src/xdpd/drivers/gnu_linux_dpdk/src/config.h
@@ -130,17 +130,15 @@
 #define GNU_LINUX_DPDK_MAX_KNI_IFACES 4
 
 /**
-* Uncomment the following line to enable the samaphore and implement a batch
-* meachanism in the DPDK secondary processes NF
+* Uncomment the following line to enable the semaphore and implement a sleep/
+* wake mechanism between xdpd and the NF process (DPDK secondary process)
 *
-* NOT recommended
+* NOT RECOMMENDED
 */
 //#define ENABLE_DPDK_SECONDARY_SEMAPHORE
-#ifdef ENABLE_DPDK_SECONDARY_SEMAPHORE
-	//IVANO - FIXME: write a meaningfull value
-	#define PKT_TO_NF_THRESHOLD 	200
-#endif
 
+//IVANO - FIXME: write a meaningfull value
+//#define PKT_TO_NF_THRESHOLD 	200
 
 
 #endif //XDPD_GNU_LINUX_XDPD_CONFIG_H

src/xdpd/drivers/gnu_linux_dpdk/src/processing/processing.cc
@@ -163,7 +163,7 @@ int processing_core_process_packets(void* not_used){
 	int j;
 	bool own_port;
 	switch_port_t* port;
-	port_queues_t* port_queues;
+	port_bursts_t* port_bursts;
         uint64_t diff_tsc, prev_tsc;
 	struct rte_mbuf* pkt_burst[IO_IFACE_MAX_PKT_BURST]={0};
 	core_tasks_t* tasks = &processing_core_tasks[rte_lcore_id()];
@@ -208,14 +208,14 @@ int processing_core_process_packets(void* not_used){
 				l++;
 
 				//make code readable
-				port_queues = &tasks->phy_ports[i];
+				port_bursts = &tasks->phy_ports[i];
 
 				//Check whether is our port (we have to also transmit TX queues)
-				own_port = (port_queues->core_id == core_id);
+				own_port = (port_bursts->core_id == core_id);
 
 				//Flush (enqueue them in the RX/TX port lcore)
 				for( j=(IO_IFACE_NUM_QUEUES-1); j >=0 ; j-- ){
-					flush_port_queue_tx_burst(phy_port_mapping[i], i, &port_queues->tx_queues_burst[j], j);
+					flush_port_queue_tx_burst(phy_port_mapping[i], i, &port_bursts->tx_queues_burst[j], j);
 
 					if(own_port)
 						transmit_port_queue_tx_burst(i, j, pkt_burst);
@@ -224,33 +224,29 @@ int processing_core_process_packets(void* not_used){
 
 #ifdef GNU_LINUX_DPDK_ENABLE_NF
 			//handle NF ports
-			for(i=0, l=0; l<total_num_of_nf_ports && likely(i<PROCESSING_MAX_PORTS) ; ++i)
-			{
+			for(i=0, l=0; l<total_num_of_nf_ports && likely(i<PROCESSING_MAX_PORTS) ; ++i){
+
 				if(!tasks->nf_ports[i].present)
 					continue;
 
 				l++;
 
-				if(nf_port_mapping[i]->type == PORT_TYPE_NF_EXTERNAL)
-				{
-					//make code readable
-					port_queues = &tasks->nf_ports[i];
+				//make code readable
+				port_bursts = &tasks->nf_ports[i];
 
+				if(nf_port_mapping[i]->type == PORT_TYPE_NF_EXTERNAL){
 					//Check whether is our port (we have to also transmit TX queues)
-					own_port = (port_queues->core_id == core_id);
+					own_port = (port_bursts->core_id == core_id);
 
-					flush_kni_nf_port_burst(nf_port_mapping[i], i, &port_queues->tx_queues_burst[0]);
+					flush_kni_nf_port_burst(nf_port_mapping[i], i, &port_bursts->tx_queues_burst[0]);
 
 					if(own_port)
-						transmit_kni_nf_port_burst(nf_port_mapping[i],i, pkt_burst);
-				}
-#ifdef ENABLE_DPDK_SECONDARY_SEMAPHORE
-				else
-				{
+						transmit_kni_nf_port_burst(nf_port_mapping[i], i, pkt_burst);
+				}else{
 					assert(nf_port_mapping[i]->type == PORT_TYPE_NF_SHMEM);
-					flush_shmem_nf_port(nf_port_mapping[i]);
+					port = nf_port_mapping[i];
+					flush_shmem_nf_port(port, ((dpdk_shmem_port_state_t*)port->platform_port_state)->to_nf_queue, &port_bursts->tx_queues_burst[0]);
 				}
-#endif
 			}
 #endif
 		}

src/xdpd/drivers/gnu_linux_dpdk/src/processing/processing.h
@@ -30,12 +30,12 @@ struct mbuf_burst {
 };
 
 //Port queues
-typedef struct port_queues{
+typedef struct port_bursts{
 	//This are TX-queues of a port
 	bool present; //signals that it is present AND is attached (usable by I/O subsytem)
 	unsigned int core_id; //core id serving RX/TX on this port
 	struct mbuf_burst tx_queues_burst[IO_IFACE_NUM_QUEUES];
-}port_queues_t;
+}port_bursts_t;
 
 /**
 * Core task list
@@ -49,11 +49,11 @@ typedef struct core_tasks{
 	switch_port_t* port_list[PROCESSING_MAX_PORTS_PER_CORE]; //active ports MUST be on the very beginning of the array, contiguously.
 	
 	//This are the TX-queues for ALL ports in the system; index is port_id
-	port_queues_t phy_ports[PROCESSING_MAX_PORTS];
+	port_bursts_t phy_ports[PROCESSING_MAX_PORTS];
 	
 #ifdef GNU_LINUX_DPDK_ENABLE_NF
 	//Only for NFs
-	port_queues_t nf_ports[PROCESSING_MAX_PORTS];
+	port_bursts_t nf_ports[PROCESSING_MAX_PORTS];
 #endif	
 }core_tasks_t;
 

commit 705edc38ce1a321bdfa16f0af09da008433811c4

Fixing shmem TX pkt routine
Fixing shmem TX routines broken in commit aaea309 during refactor

src/xdpd/drivers/gnu_linux_dpdk/src/io/tx.h
@@ -241,6 +241,8 @@ tx_pkt_shmem_nf_port(switch_port_t* port, datapacket_t* pkt)
 		flush_shmem_nf_port(port, port_state->to_nf_queue, pkt_burst);
 		return;
 	}
+
+	pkt_burst->len = len;
 }
 
 /**